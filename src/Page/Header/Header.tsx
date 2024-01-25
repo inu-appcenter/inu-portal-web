93e4ff9ece2d77f65188a513eac34d17b0739191
seokahi <skh9797@naver.com>
2024-01-25 21:24:12 +0900

    feat: 헤더 추가

---
+++ src/Page/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+function Header() {
+    return (
+        <header className="header-header">
+            <a href="">로그인</a>
+            <button>English</button>
+        </header>
+    )
+}
+
+export default Header
