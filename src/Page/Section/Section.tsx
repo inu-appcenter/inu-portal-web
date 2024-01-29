ecc5efbd7356d3e4d5ef70df3b192b2192dba079
seokahi <skh9797@naver.com>
2024-01-30 02:21:34 +0900

    desing : 각 페이지 css 변경

--- src/Page/Section/Section.tsx
+++ src/Page/Section/Section.tsx
@@ -1,9 +1,12 @@
 import img from "../../assets/Images/image 7.png"
+import DashBoard from "./Calendar/Calendar"
 import "./Section.css"
+
 function Section() {
 return (
     <>
     <img className="homepage-img" src={img} alt="" />
+    <DashBoard/>
     </>
 )
 }
