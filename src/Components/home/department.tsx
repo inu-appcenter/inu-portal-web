8fc1bf9a17d3f65e96827ba6525250de9c68271a
seokahi <skh9797@naver.com>
2024-01-31 15:37:19 +0900

    refactor: 학교홈페이지 component/home으로 이동

--- src/Components/home/department.tsx
+++ src/Components/home/department.tsx
@@ -1,7 +1,9 @@
+import "styled-components"; 
 
 import CategoryDepartment from "../../Page/Category/CategoryDepartment";
 import { departmentType } from "../../Resource/Data/aboutDepartment";
 import { useState } from "react";
+import { BannerDepartment } from "../../Styles/department";
 
 export default function Department() {
     
@@ -9,13 +11,17 @@ export default function Department() {
 
     const onDepartmentToggle = () => {
         setIsDepartmentOpen(!isDepartmentOpen);
-      };
+    };
+    
     return (
         <li>
-              <div onClick={onDepartmentToggle}>
-                <p className={`banner-department${isDepartmentOpen ? ' open' : ''}`}>학과 홈페이지</p>
-              </div>
-              {isDepartmentOpen && <CategoryDepartment department={departmentType} />}
+            <div onClick={onDepartmentToggle}>
+            <BannerDepartment className={`banner-department${isDepartmentOpen ? ' open' : ''}`}>학과 홈페이지</BannerDepartment>
+            </div>
+            {isDepartmentOpen && <CategoryDepartment department={departmentType} />}
         </li>
-    )
+        );
+    
+    
 }
+
---
+++ src/Styles/department.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const BannerDepartment = styled.p`
+    padding: 55px;
+    &.open {
+        box-sizing: border-box;
+        background-color: #424242;
+        color: white;
+    }
+`;
+
+export const BannerListItem = styled.li`
+    font-size: 14px;
+    font-weight: 900;
+`;
