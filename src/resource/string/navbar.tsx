f589d9a0fc7b5b7858d961e60674a0d07e4eb9f6
seokahi <skh9797@naver.com>
2024-02-01 17:00:11 +0900

    refactor: asset(이미지), string(데이터) 폴더 추가

---
+++ src/resource/string/navbar.tsx
@@ -0,0 +1,26 @@
+export const navBarList = [
+    {
+        title:'메인 페이지',
+    },
+    {
+        title:'학과 페이지',
+        child:[
+            {title: '메인 페이지'},
+            {title: '학과 페이지'},
+            {title: '학교 페이지'},
+            {title: '마이 페이지'}
+        ]
+    },
+    {
+        title:'학교 페이지',
+        child:[
+            {title: '메인 페이지'},
+            {title: '학과 페이지'},
+            {title: '학교 페이지'},
+            {title: '마이 페이지'}
+        ]
+    },
+    {
+        title:'마이 페이지',
+    }
+]
