ad214c83056926d6c44ceb3b2d17168eb1af2022
seokahi <skh9797@naver.com>
2024-02-04 21:31:36 +0900

    feat : utilbutton 추가

--- src/container/login/LoginDetailContainer.tsx
+++ src/container/login/LoginDetailContainer.tsx
@@ -1,7 +1,9 @@
+
 import styled from 'styled-components';
 import LoginTitle from '../../component/login/logintitle';
 import LoginInput from '../../component/login/logininput';
 import UtilButtons from '../../component/login/utilbuttons';
+
 export default function LoginDetail () {
 
   return (
