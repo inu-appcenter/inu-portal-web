21841dff82683fb20aee7709f5080acbc6a83cc2
seokahi <skh9797@naver.com>
2024-02-13 23:10:00 +0900

    feat: 스크랩한 글 가져오기 - 서버 API 연동

---
+++ src/utils/getScrap.tsx
@@ -0,0 +1,29 @@
+const getScrapInfo = async (token:string) => {
+    console.log(token,".");
+    const apiURL = `https://portal.inuappcenter.kr/api/members/scrap`;
+    try {
+        const response = await fetch(apiURL, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Auth':token
+            },
+        });
+
+        console.log(response, 'response');
+        if (response.status == 400) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        } else {
+            const data = await response.json();
+            console.log(data);
+            return data;
+        }
+
+
+    } catch (error) {
+        console.log("에러?", error);
+        throw error;
+    }
+};
+
+export default getScrapInfo;
