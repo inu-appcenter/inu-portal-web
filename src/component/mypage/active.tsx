59920839545907da72c10056a7822dd4bfa67ea7
seokahi <skh9797@naver.com>
2024-02-13 23:10:29 +0900

    feat: 내 활동 페이지 생성(수정예정)

---
+++ src/component/mypage/active.tsx
@@ -0,0 +1,22 @@
+
+import styled from 'styled-components';
+
+
+export default function ActiveInfo() {
+    
+
+  return (
+    <ScrapWrapper>
+      <ScrapItem>내 활동은</ScrapItem>
+    </ScrapWrapper>
+  );
+}
+
+const ScrapWrapper = styled.div`
+
+
+`;
+
+const ScrapItem = styled.div`
+
+`;
