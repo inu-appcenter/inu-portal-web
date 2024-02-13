de2ce991c97f2c174adf5994ca0362b94f1fed38
seokahi <skh9797@naver.com>
2024-02-13 23:08:03 +0900

    feat: 마이페이지 컨테이너 생성

---
+++ src/container/mypage/MyPageContainer.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import MyPageListButton from '../../component/mypage/listbutton';
+
+
+export default function MyPageContainer() {
+  return (
+    <MyPageWrapper>
+      <MyPageListButton />
+    </MyPageWrapper>
+  );
+}
+
+const MyPageWrapper = styled.div`
+
+`;
---
+++ src/resource/string/mypage.tsx
@@ -0,0 +1,21 @@
+import scrapImg from "../assets/search.svg"
+
+export const MypageList = [
+    {
+        title:'스크랩',
+        img:scrapImg
+    },
+    {
+        title:'내 활동',
+        img:scrapImg
+    },
+    {
+        title:'비밀번호 변경',
+        img:scrapImg
+    },
+    {
+        title:'회원탈퇴',
+        img:scrapImg
+    }
+]
+
