9c7913c751caa2f67bccb6d1b040f4bc2ad70172
seokahi <skh9797@naver.com>
2024-02-15 15:22:44 +0900

    feat: 토큰 없을 시 홈페이지로 이동

--- src/container/mypage/MyPageContainer.tsx
+++ src/container/mypage/MyPageContainer.tsx
@@ -1,8 +1,19 @@
 import styled from 'styled-components';
 import MyPageListButton from '../../component/mypage/listbutton';
-
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function MyPageContainer() {
+  const token = useSelector((state: any) => state.user.token);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (!token) {
+      navigate('/home');
+    }
+  }, [token, navigate]);
+
   return (
     <MyPageWrapper>
       <MyPageListButton />
