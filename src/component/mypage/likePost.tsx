9a0d60b2975caa6120a00a433a6efda41d16fee3
seokahi <skh9797@naver.com>
2024-02-22 10:41:08 +0900

    style: 좋아요 누른 글 style 추가

--- src/component/mypage/likePost.tsx
+++ src/component/mypage/likePost.tsx
@@ -1,39 +1,90 @@
 import styled from 'styled-components';
-
 import { Link } from 'react-router-dom';
+import likelogo from '.././../resource/assets/like-icon.png';
 
 interface postinfoProps {
   postLikeInfo: {
-        id: number;
-        title: string;
-        category: string;
-    }[];
+    id: number;
+    title: string;
+    category: string;
+  }[];
 }
 
-export default function LikePost({postLikeInfo}:postinfoProps) {
-    
+export default function LikePost({ postLikeInfo }: postinfoProps) {
   return (
-    <ScrapWrapper>
-    {postLikeInfo.map((item) => (
-        <ScrapItem key={item.id}>
-        <h3>{item.title}</h3>
-        <p>{item.category}</p>
-        {/* 아래의 부분 수정 */}
-        <PoistLink to={`/post/${item.id}`}>자세히 보기</PoistLink>
-        </ScrapItem>
-    ))}
-</ScrapWrapper>
+    <LikePostWrapper>
+      <CountWrapper>
+        <Likeimg src={likelogo} />
+        <LikeCount>{postLikeInfo.length}</LikeCount>
+      </CountWrapper>
+      <Items>
+        {postLikeInfo.map((item) => (
+          <PostLink  to={`/tips/${item.id}`}>
+            <LikePostItem key={item.id}>
+              <p className='category'>{item.category}</p>
+              <p className='title'>{item.title}</p>
+          </LikePostItem>
+          </PostLink>
+         
+        ))}
+      </Items>
+    </LikePostWrapper>
   );
 }
 
-const ScrapWrapper = styled.div`
+const LikePostWrapper = styled.div`
 
 `;
 
-const ScrapItem = styled.div`
-  
-`;
-
-const PoistLink = styled(Link)`
-  
+const CountWrapper = styled.div`
+  display: flex;
+  margin-top: 26px;
+  align-items: center;
 `
+const Likeimg = styled.img`
+  width: 24px;
+  height: 20px;
+`;
+
+const LikeCount = styled.p`
+font-family: Inter;
+font-size: 15px;
+font-weight: 600;
+margin-left: 15px;
+`;
+
+const Items = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  max-height: 150px; 
+  overflow-y: auto; 
+`;
+
+const LikePostItem = styled.div`
+  display: flex;
+  gap:2px;
+  background-color:white;
+  .category {
+    background-color: #a4c8e4; 
+    padding:10px;
+    margin:10px;
+    color:white;
+    font-weight: 600;
+    border-radius: 10px;
+  }
+  .title {
+    padding:10px;
+    margin:10px;
+    color:black;
+    font-weight: 600;
+    border-radius: 10px;
+  }
+
+`;
+
+const PostLink = styled(Link)`
+    text-decoration:none;
+  color:black;
+  box-sizing: border-box;
+`;
