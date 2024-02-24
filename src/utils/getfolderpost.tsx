e60a78f16323e6efeb73f89a86cfd505c59d5c8a
seokahi <skh9797@naver.com>
2024-02-24 20:46:11 +0900

    feat: 스크랩폴더의 모든 게시글 가져오기 API 추가

---
+++ src/utils/getfolderpost.tsx
@@ -0,0 +1,25 @@
+const getFolderPost = async (folderId: number) => {
+    const apiURL = `https://portal.inuappcenter.kr/api/folders/${folderId}`;
+    try {
+      const response = await fetch(apiURL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+  
+      console.log(response,'response');
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log(data);
+        return data.data;
+      } else {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+    } catch (error) {
+      console.log("에러?", error);
+      throw error;
+    }
+  };
+  
+  export default getFolderPost;
