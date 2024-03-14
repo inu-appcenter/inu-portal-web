3e7305c013b6a42eac2e854fc9f77f7e8cf43e6b
So Jeong <7658641@naver.com>
2024-03-14 17:56:25 +0900

    feat: 글쓰기 접근 시 로그인 여부 확인

--- src/component/Tips/PostButton.tsx
+++ src/component/Tips/PostButton.tsx
@@ -2,13 +2,41 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import pencilImg from '../../resource/assets/Pencil.png';
 
-export default function RegisterInput() {
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import LoginModal from '../common/LoginModal';
+
+interface loginInfo {
+  user: {
+    token: string;
+  };
+}
+export default function PostBtn() {
   const navigate = useNavigate();
+  const user = useSelector((state: loginInfo) => state.user);
+  const [isOpenModal, setOpenModal] = useState<boolean>(false);
+  
+  const handlePostClick = () => {
+    if (!user.token) {
+      // 사용자가 로그인되어 있지 않으면 모달을 열기
+      setOpenModal(true);
+    } else {
+      // 사용자가 로그인되어 있으면 글쓰기 페이지로 이동
+      navigate('/write');
+    }
+  };
+
+  const closeModal = () => {
+    setOpenModal(false); // 모달을 닫기 위해 상태 업데이트
+};
 
   return (
-    <PostButtonWrapper onClick={() => {navigate('/write')}}>
-      <img src={pencilImg} alt='pencilImg'></img>
+    <>
+    <PostButtonWrapper onClick={handlePostClick}>
+      <img src={pencilImg} alt='pencilImg'/>
     </PostButtonWrapper>
+     {isOpenModal && <LoginModal setOpenModal={setOpenModal} closeModal={closeModal} />}
+     </>
   )
 }
 
