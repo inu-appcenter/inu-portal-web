fa0e07bbfbc99bdba0bf472dddb6b46f2ec9bd2f
seokahi <skh9797@naver.com>
2024-03-19 14:36:22 +0900

    feat: ProfileuserAction 리덕스 함수 추가

---
+++ src/container/mypage/MyPageTitleContainer.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import getUser from '../../utils/getUser';
+import { NicknameUser as NicknameUserAction , ProfileUser as ProfileuserAction} from '../../reducer/userSlice';
+import { MypageTitle } from '../../component/mypage/common/MyPageTitle';
+import MyPageUserInfo from '../../component/mypage/common/MyPageUserInfo';
+
+
+export default function MyPageTitleContainer() {
+  const token = useSelector((state: any) => state.user.token);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!token) {
+      navigate('/home');
+    }
+  }, [token, navigate]);
+
+    useEffect(() => {
+        handleUserInfo();
+    }, []);
+
+
+  
+    const handleUserInfo = async () => {
+        try {
+            const response = await getUser(token);
+            console.log(response,"결과뭐야");
+            dispatch(NicknameUserAction({"nickname":response.nickname}));
+            dispatch(ProfileuserAction({"fireId":response.fireId}));
+        } catch (error) {
+            console.error("회원을 가져오지 못했습니다.", error);
+        }
+    };
+
+  return (
+    <MyPageTitleWrapper>
+      <MypageTitle/>
+      <MyPageUserInfo/>
+    </MyPageTitleWrapper>
+  );
+}
+
+const MyPageTitleWrapper = styled.div`
+    display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px;
+`;
+
