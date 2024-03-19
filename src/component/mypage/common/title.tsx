07b1defabbc7dfa3854d12eee42f329ce6e928bb
seokahi <skh9797@naver.com>
2024-03-19 14:32:04 +0900

    refactor: title 공통 폴더 추가

---
+++ src/component/mypage/common/title.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+interface TitleProps {
+    title: string;
+}
+
+// React.FC 타입에 TitleProps 추가
+const Title: React.FC<TitleProps> = ({ title }) => {
+    return (
+        <TitleInfo>{title}</TitleInfo>
+    );
+};
+
+const TitleInfo = styled.div`
+  color: #0E4D9D;
+  font-family: Inter;
+  font-size: 30px;
+  font-weight: 600;
+  margin-bottom: 72px;
+  width: 420px;
+`;
+
+export default Title;
