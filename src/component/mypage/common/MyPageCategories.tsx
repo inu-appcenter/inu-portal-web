011080b9ab63a38b5312f3ca463e5b01487369d0
mingyu <tnt01010@naver.com>
2024-03-28 20:31:03 +0900

    style: 마이페이지 카테고리 아이콘

--- src/component/mypage/common/MyPageCategories.tsx
+++ src/component/mypage/common/MyPageCategories.tsx
@@ -4,28 +4,59 @@ import { useNavigate } from "react-router-dom";
 // import './TipsCategories.css';
 import { MyPageCategory } from '../../../resource/string/mypage';
 
+interface Category {
+  name: string;
+  iconWhite: string;
+  iconGray: string;
+  hasError?: boolean;
+}
+
 interface MyPageCategoriesProps {
   selectedCategory: string;
   setSelectedCategory: (category: string) => void;
 }
 
 export default function MyPageCategories({ selectedCategory, setSelectedCategory }: MyPageCategoriesProps) {
-  const [categories, setCategories] = useState<string[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
   const navigate = useNavigate();
 
   const fetchCategories = async () => {
-    setCategories(MyPageCategory);
+    const cats = MyPageCategory.map((cat: string) => ({
+      name: cat,
+      iconWhite: `/src/resource/assets/categoryIcons/${cat}_white.svg`,
+      iconGray: `/src/resource/assets/categoryIcons/${cat}_gray.svg`
+    }));
+    setCategories(cats);
   };
 
   useEffect(() => {
     fetchCategories();
   }, []);
 
+  // 이미지 로드 실패 시 호출될 핸들러
+  const handleImageError = (index: number) => {
+    setCategories(prevCategories => prevCategories.map((cat, idx) => idx === index ? { ...cat, hasError: true } : cat));
+  };
+  
+  const handleClickCategory = (category: string) => {
+    setSelectedCategory(category);
+    navigate(`/mypage`);
+  };
+
   return (
     <div className='categories'>
       {categories.map((category, index) => (
-        <div className={`categoryItem ${selectedCategory === category ? 'selected' : ''}`} key={index} onClick={() => {setSelectedCategory(category); navigate(`/mypage`);}}>
-          {category}
+        <div className={`categoryItem ${selectedCategory === category.name ? 'selected' : ''}`} key={index} onClick={() => handleClickCategory(category.name)}>
+          {category.hasError ? (
+            <div style={{ width: '25px', height: '25px' }}> {/* 이미지 로드 실패 시 공백을 위한 div */}</div>
+          ) : (
+            <img 
+              src={selectedCategory === category.name ? category.iconWhite : category.iconGray} 
+              alt={category.name} 
+              onError={() => handleImageError(index)}
+            />
+          )}
+          {category.name}
         </div>
       ))}
     </div>
