c1b805e194419dda31df62301cdb7e1f4b9c60c0
mingyu <tnt01010@naver.com>
2024-04-04 21:42:45 +0900

    style: 댓글, 대댓글

--- src/component/postdetail/comment/commentlist.tsx
+++ src/component/postdetail/comment/commentlist.tsx
@@ -38,36 +38,37 @@ const CommentList: React.FC<CommentListProps> = ({ bestComment, comments, onComm
       <div className='commentlist-container'>
       {allComments.map((comment, index) => (
         <div key={comment.id} className='comment-container'>
-          <div className='profile-image-background'>
-            <img alt='프'/>
-          </div>
-          <div className='comment-container-mid'>
-            <div>
-              <span className='writer-text'>{comment.writer} </span>
-              {index === 0 && bestComment && (<span className='best-text'>Best</span>)}
-            </div> 
-            <span className='content-text'>{comment.content}</span>
-            <div className='commentUtility-container'>
-              <span className='commentUtility' onClick={() => setShowReCommentInputId(comment.id)}>답장</span>
-              {comment.hasAuthority && (
-              <>
-                <EditCommentButton token={token} id={comment.id} currentContent={comment.content} isAnonymous={comment.isAnonymous} onCommentUpdate={onCommentUpdate} />
-                <DeleteCommentButton token={token} id={comment.id} onCommentUpdate={onCommentUpdate} />
-              </>
-              )}
+          <div className='comment-main-container'>
+            <div className='profile-image-background'>
+              <img alt='프'/>
+            </div>
+            <div className='comment-container-mid'>
+              <div>
+                <span className='writer-text'>{comment.writer} </span>
+                {index === 0 && bestComment && (<span className='best-text'>Best</span>)}
+              </div> 
+              <span className='content-text'>{comment.content}</span>
+              <div className='commentUtility-container'>
+                <span className='commentUtility' onClick={() => setShowReCommentInputId(comment.id)}>답장</span>
+                {comment.hasAuthority && (
+                <>
+                  <EditCommentButton token={token} id={comment.id} currentContent={comment.content} isAnonymous={comment.isAnonymous} onCommentUpdate={onCommentUpdate} />
+                  <DeleteCommentButton token={token} id={comment.id} onCommentUpdate={onCommentUpdate} />
+                </>
+                )}
+              </div>
+            </div>
+            <div className='comment-container-right'>
+              <div className='commentUtility'>{comment.createDate}</div>
+              <CommentLike id={comment.id} like={comment.like} isLikedProp={comment.isLiked} />
             </div>
           </div>
-          <div className='comment-container-right'>
-            <div className='commentUtility'>{comment.createDate}</div>
-            <CommentLike id={comment.id} like={comment.like} isLikedProp={comment.isLiked} />
+            {showReCommentInputId === comment.id && (
+              <ReCommentInput parentId={comment.id} onCommentUpdate={onCommentUpdate} />
+            )}
+          <div>
+            {comment.reReplies && (<ReCommentList token={token} reReplies={comment.reReplies} onCommentUpdate={onCommentUpdate} />)}
           </div>
-          {
-            /* 
-          {showReCommentInputId === comment.id && (
-            <ReCommentInput parentId={comment.id} onCommentUpdate={onCommentUpdate} />
-          )}
-          {comment.reReplies && (<ReCommentList token={token} reReplies={comment.reReplies} onCommentUpdate={onCommentUpdate} />)}*/
-          }
         </div>
       ))}
       </div>
--- src/component/postdetail/comment/recommentinput.tsx
+++ src/component/postdetail/comment/recommentinput.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import postReComment from "../../../utils/postReComment";
+import './recommentinput.css';
+import checkedCheckbox from '../../../resource/assets/checked-checkbox.png';
+import uncheckedCheckbox from '../../../resource/assets/unchecked-checkbox.png';
+import enterImage from '../../../resource/assets/enter-image.png';
 
 interface ReCommentInputProps {
   parentId: number;
@@ -11,6 +15,8 @@ export default function ReCommentInput({ parentId, onCommentUpdate }: ReCommentI
   const token = useSelector((state: any) => state.user.token);
   const [content, setContent] = useState("");
   const [isAnonymous, setIsAnonymous] = useState(false);
+
+  const toggleAnonymous = () => setIsAnonymous(!isAnonymous);
 
   const handleReCommentSubmit = async () => {
     if (!content.trim()) {
@@ -35,22 +41,33 @@ export default function ReCommentInput({ parentId, onCommentUpdate }: ReCommentI
     }
   };
 
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      handleReCommentSubmit();
+    }
+  };
+
   return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          checked={isAnonymous}
-          onChange={(e) => setIsAnonymous(e.target.checked)}
-        />
-        익명
-      </label>
-      <textarea
+    <div className="recomment-input-container">
+      <img
+        src={isAnonymous ? checkedCheckbox : uncheckedCheckbox}
+        alt="Anonymous"
+        onClick={toggleAnonymous} 
+      />
+      <span className='recomment-anonymous-text'>익명</span>
+      <div className='recomment-vline'></div>
+      <input
+        className='recomment-input'
         value={content}
         onChange={(e) => setContent(e.target.value)}
+        onKeyDown={handleKeyPress}
         placeholder="대댓글을 입력하세요."
       />
-      <button onClick={handleReCommentSubmit}>대댓글 등록</button>
+      <img
+        src={enterImage}
+        alt='enter'
+        onClick={handleReCommentSubmit}
+      />
     </div>
   );
 }
