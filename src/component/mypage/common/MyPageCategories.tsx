19cbc75e166eedf939eaa3a869c5843fb18e5af7
mingyu <tnt01010@naver.com>
2024-04-08 11:12:53 +0900

    feat: categoryIcons폴더 public으로 이동

--- src/component/mypage/common/MyPageCategories.tsx
+++ src/component/mypage/common/MyPageCategories.tsx
@@ -24,8 +24,8 @@ export default function MyPageCategories({ selectedCategory, setSelectedCategory
   const fetchCategories = async () => {
     const cats = MyPageCategory.map((cat: string) => ({
       name: cat,
-      iconWhite: `/assets/categoryImages/${cat}_white.svg`,
-      iconGray: `/assets/categoryImages/${cat}_gray.svg`
+      iconWhite: `/categoryIcons/${cat}_white.svg`,
+      iconGray: `/categoryIcons/${cat}_gray.svg`
     }));
     setCategories(cats);
   };
