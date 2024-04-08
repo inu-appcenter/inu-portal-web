130fc29cd89165aa19ef23a64a21288e520a66c6
mingyu <tnt01010@naver.com>
2024-04-09 00:57:23 +0900

    EditPost, CreatePost -> WritePost로 통합

---
+++ src/page/WritePostPage.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import PostFormContainer from '../container/writepost/PostFormContainer';
+
+export default function WritePost() {
+  return (
+    <WritePostWrapper>
+      <PostFormContainer />
+    </WritePostWrapper>
+  );
+}
+
+const WritePostWrapper = styled.div`
+
+  display: flex;
+  justify-content: center;
+`;
