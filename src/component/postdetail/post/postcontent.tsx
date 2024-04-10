a0dca43b3ed4f7f3307925ec10c96b87dbb05dd1
mingyu <tnt01010@naver.com>
2024-04-10 15:13:04 +0900

    style: postcontent 이미지 크기 조절, 이미지 모달

--- src/component/postdetail/post/postcontent.tsx
+++ src/component/postdetail/post/postcontent.tsx
@@ -8,8 +8,22 @@ interface PostContentProps {
   imageCount: number;
 }
 
+const ImageModal = ({ src, onClose }: {src: string; onClose: () => void}) => {
+  return (
+    <div className='image-modal'>
+      <div className='modal-button-wrapper'>
+        <div className='modal-button-red' onClick={onClose}></div>
+        <div className='modal-button-green' onClick={() => window.open(src, '_blank')}></div>
+      </div>
+      <img src={src} alt='Modal' onClick={onClose} />
+    </div>
+  )
+}
+
 export default function PostContent({ id, content, imageCount }: PostContentProps) {
   const [images, setImages] = useState<string[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -29,15 +43,21 @@ export default function PostContent({ id, content, imageCount }: PostContentProp
     fetchImages();
   }, [id, imageCount]);
 
+  const handleImageClick = (imageSrc: string) => {
+    setSelectedImage(imageSrc);
+    setShowModal(true);
+  };
+
   return (
     <>
       <div className='post-contents'>
         <div className='contents-img-container'>
         {images.map((image, index) => (
-          <img className='contents-img' key={index} src={image} alt={`Image ${index + 1}`} />
+          <img className='contents-img' key={index} src={image} alt={`Image ${index + 1}`} onClick={() => handleImageClick(image)}/>
         ))}
         </div>
         <div className='contents'>{content}</div>
+        {showModal && <ImageModal src={selectedImage} onClose={() => setShowModal(false)} />}
       </div>
     </>
   );
