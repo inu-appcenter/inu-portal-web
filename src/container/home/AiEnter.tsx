b99aa77112677f1100122e930e30036d0bcb6a0f
mingyu <tnt01010@naver.com>
2024-05-02 01:58:08 +0900

    fix: 배포 오류 잡기

--- src/container/home/AiEnter.tsx
+++ src/container/home/AiEnter.tsx
@@ -1,8 +1,4 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-
 export default function AIEnter() {
-    const navigate = useNavigate()
     const handleAiBtnClick = () => { // 매개변수로 url을 추가하여 해당 URL을 사용할 수 있도록 함
         window.open(`/ai`);
       };
