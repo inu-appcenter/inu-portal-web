5b512504c8d772a58fe5b7b7e38c11a313120669
mingyu <tnt01010@naver.com>
2024-05-06 16:54:05 +0900

    fix: 배포 오류 해결

--- src/component/ai/AiImgViewer.tsx
+++ src/component/ai/AiImgViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import imgshare from '../../resource/assets/imgshare.svg'
 import imgsave from '../../resource/assets/imgsave.svg'
