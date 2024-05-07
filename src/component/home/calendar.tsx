0b32a29dce64adeb3615496e355b87a3eb5ab0c8
seokahi <skh9797@naver.com>
2024-05-07 11:23:52 +0900

    feat: 캘린더 api 불러오는 기능 추가 및 현재 날짜 조회 기능 추가

--- src/component/home/calendar.tsx
+++ src/component/home/calendar.tsx
@@ -2,26 +2,48 @@ import { useRef, useEffect, useState } from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import FullCalendar from "@fullcalendar/react";
 import { EventInput } from "@fullcalendar/core/index.js";
-import { calendarInfo } from "../../resource/string/calendar";
+
+import getCalendar from "../../utils/getCalendar";
+import "./calendar.css";
 
 export default function Calendarbar() {
   const calendarRef = useRef<FullCalendar>(null);
   const [events, setEvents] = useState<EventInput[]>([]);
+  const [month, setMonth] = useState<number>(undefined);
+  const [year, setYear] = useState<number>(undefined);
 
   useEffect(() => {
-    addInitialEvents();
+    getCurrentMonth();
   }, []);
 
-  const addInitialEvents = () => {
-    // Instead of direct assignment, use setEvents function to update the state
-    setEvents(calendarInfo);
-    console.log(events);
+  useEffect(() => {
+    fetchCalendarData();
+  }, [year, month]);
+
+  const fetchCalendarData = async () => {
+    try {
+      const response = await getCalendar(year, month);
+      console.log(response, "return 값");
+      setEvents(response.data);
+    } catch (error) {
+      console.error("학식 정보 조회 안됨");
+    }
+  };
+
+  const getCurrentMonth = () => {
     const calendarApi = calendarRef.current?.getApi();
     if (calendarApi) {
-      calendarInfo.forEach((event) => {
-        calendarApi.addEvent(event);
-      });
+      const currentDate = calendarApi.getDate();
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth() + 1;
+      setYear(year);
+      setMonth(month);
+      console.log("현재 month", month, year);
     }
+  };
+
+  const handleDateChange = () => {
+    getCurrentMonth();
   };
 
   return (
@@ -30,7 +52,9 @@ export default function Calendarbar() {
       plugins={[dayGridPlugin]}
       initialView={"dayGridMonth"}
       headerToolbar={{ start: "title", end: "prev,next" }}
-      eventTextColor='gray'
+      eventTextColor="gray"
+      datesSet={handleDateChange}
+      events={events} 
     />
   );
 }
