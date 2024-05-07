b09abd46d68fa0358a1db4354f1138168ffe3a90
seokahi <skh9797@naver.com>
2024-05-07 11:20:28 +0900

    feat: 학식 정보 데이터 추가

---
+++ src/resource/string/cafeterias.tsx
@@ -0,0 +1,7 @@
+export const cafeteriasList = [
+    {id:0, title:'학생식당'},
+    {id:1, title:'제1기숙사식당'},
+    {id:2, title:'2호관(교직원)식당'},
+    {id:3, title:'27호관식당'},
+    {id:4, title:'사범대식당'},
+];
