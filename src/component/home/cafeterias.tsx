3f940d398efaaf626cb1d699f3ca06fa2ebbdf04
So Jeong <7658641@naver.com>
2024-05-10 17:47:40 +0900

    style: 학식 메뉴 디자인 수정

--- src/component/home/cafeterias.tsx
+++ src/component/home/cafeterias.tsx
@@ -43,12 +43,12 @@ export default function Cafeteria() {
   return (
     <>
       <CafeteriaWrapper>
-        <p className="date">{currentDate}</p>
                 <div className="title">
                 <div className="circle"></div>
-                <h1 className="cafeteria-type">{cafeteriaType}</h1>
+                <div className="cafeteria-type">{cafeteriaType}</div>
                 <h1 className="today">Today</h1>
                 </div>
+                <span className="date">{currentDate}</span>
                 <div className="cafeteria-list">
                 <CafetriaInfo>
                 <div className="breakfast">
@@ -88,7 +88,7 @@ export default function Cafeteria() {
 
         {cafeteriasList.map((cafeteria) => (
           <Cafeterias className="cafeteria"key={cafeteria.id} onClick={() => handleCafeteriaType(cafeteria.title,cafeteria.info)}>
-            <p className="cafeteria-title">{cafeteria.title}</p>
+            <div className="cafeteria-title">{cafeteria.title}</div>
           </Cafeterias>
         ))}
          </CafetriaType>
@@ -101,12 +101,13 @@ export default function Cafeteria() {
 const CafeteriaWrapper = styled.div`
   width: 550px;
   box-sizing: border-box;
-  border-radius: 2px;
+  border-radius: 10px;
   border: 2px solid transparent;
   box-shadow: 0 0 0 2px #6f84e2 inset;
-  padding: 12px 14px 25px 16px;
+  padding: 20px 18px;
+
   .date {
-    font-size: 9px;
+    font-size: 12px;
     color: #656565;
     display: flex;
     justify-content: flex-end; 
@@ -117,17 +118,17 @@ const CafeteriaWrapper = styled.div`
   }
 
   .circle {
-    width: 10px;
-    height: 10px;
+    width: 15px;
+    height: 15px;
     border-radius: 50%;
     border: 2px solid transparent;
     box-shadow: 0 0 0 2px #85cbda inset;
-    margin-right: 8px; 
+    margin-right: 10px; 
   }
 
   .cafeteria-type {
     font-family: inter;
-    font-size: 12px;
+    font-size: 20px;
     font-weight: bold;
   }
 
@@ -141,6 +142,7 @@ const CafeteriaWrapper = styled.div`
   .cafeteria-list{
     display: flex;
     justify-content: space-between;
+    gap:10px;
   }
 `;
 
@@ -157,14 +159,20 @@ const CafetriaInfo = styled.div`
             display: flex;
             align-items: center;
 
-            img {
+            .img {
             width:25px;
             height:25px;
         }
+
+            .wrapper{
+              
+            }
+
             .type {
                 font-family: inter ;
                 font-size: 8px;
                 color:#969696;
+               
                 font-weight: bold;
                 margin:0;
             }
@@ -190,6 +198,7 @@ const CafetriaType = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-end;
+    justify-content: space-between;
 `
 
 const Cafeterias  = styled.div`
@@ -198,10 +207,13 @@ const Cafeterias  = styled.div`
   font-size: 10px;
   line-height: 25px;
   text-align:center;
+  color:#fff;
+
   .cafeteria-title {
     padding:2px 20px;
     background:linear-gradient(90deg, #83CBD9 0%, #99C7E1 50%, #A1C9F1 100%);
     border-radius: 5px;
     width: 93px;
+    margin: 5px;
   }
 `
