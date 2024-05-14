3fcdefc08cda1966600d96a18ef0083db196fb7d
mingyu <tnt01010@naver.com>
2024-05-14 13:36:11 +0900

    feat: footer 추가

---
+++ src/component/common/FooterBottom.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import CopyRightText from '../../resource/assets/CopyRightText.svg';
+import phoneImg from '../../resource/assets/phoneImg.svg';
+
+export default function FooterBottom() {
+  return (
+    <FooterBottomWrapper>
+      <img src={CopyRightText} />
+      <ContactWrapper>
+        <img src={phoneImg} />
+        <span>Contact</span>
+        <span>032-000-0092</span>
+      </ContactWrapper>
+    </FooterBottomWrapper>
+  )
+}
+
+const FooterBottomWrapper = styled.div`
+  width: 80%;
+  margin: 10px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  color: #969696;
+  font-weight: 700;
+`
+
+const ContactWrapper = styled.span`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`
---
+++ src/container/common/FooterContainer.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import FooterTop from "../../component/common/FooterTop";
+import FooterBottom from "../../component/common/FooterBottom";
+
+export default function Footer() {
+  return (
+    <FooterWrapper>
+      <FooterTop />
+      <FooterLine />
+      <FooterBottom />
+    </FooterWrapper>
+  )
+}
+
+const FooterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const FooterLine = styled.div`
+  width: 90%;
+  height: 0px;
+  border: 2px solid #BEBEBE
+`
