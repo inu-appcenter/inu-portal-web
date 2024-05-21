9f0763cbaad8fb5f728963ef7f5d59edc327a267
mingyu <tnt01010@naver.com>
2024-05-21 19:24:23 +0900

    style: 로그인 페이지 이미지

---
+++ src/component/login/LoginTipImage.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import LoginTipImg1 from '../../resource/assets/LoginTipImg1.svg';
+import LoginTipImg2 from '../../resource/assets/LoginTipImg2.svg';
+import LoginTipImg3 from '../../resource/assets/LoginTipImg3.svg';
+import LoginTipImg4 from '../../resource/assets/LoginTipImg4.svg';
+
+const images = [LoginTipImg1, LoginTipImg2, LoginTipImg3, LoginTipImg4];
+
+export default function LoginTipImage() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <LoginTipImageComponent src={images[currentImageIndex]} key={currentImageIndex} />
+      <Indicators>
+        {images.map((_, index) => (
+          <Indicator key={index} isActive={index === currentImageIndex} />
+        ))}
+      </Indicators>
+    </>
+  );
+}
+
+const fadein = keyframes`
+  from { opacity: 0; }
+  to { opacity: 1; }
+`;
+
+const fadeout = keyframes`
+  from { opacity: 1; }
+  to { opacity: 0; }
+`;
+
+const LoginTipImageComponent = styled.img`
+  z-index: 3;
+  position: absolute;
+  left: 50%;
+  top: 180px;
+  transform: translateX(-50%);
+  width: 90%;
+  max-height: calc(100% - 200px);
+  object-fit: contain;
+  animation: ${fadein} 1s, ${fadeout} 1s 4s;
+`;
+
+const Indicators = styled.div`
+  z-index: 4;
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  border-radius: 8px;
+  background-color: rgba(0,0,0,0.25);
+  padding: 5px;
+`;
+
+const Indicator = styled.div<{ isActive: boolean }>`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: ${({ isActive }) => (isActive ? 'black' : 'white')};
+`;
--- src/container/login/LoginDetailContainer.tsx
+++ src/container/login/LoginDetailContainer.tsx
@@ -7,8 +7,10 @@ export default function LoginDetail () {
 
   return (
     <LoginDetaileWrapper>
+      <LoginDetailItemWrapper>
         <LoginTitle/>
         <LoginInput/>
+      </LoginDetailItemWrapper>
         {/* <UtilButtons/> */}
     </LoginDetaileWrapper>
   );
@@ -17,13 +19,28 @@ export default function LoginDetail () {
 
 const LoginDetaileWrapper = styled.div`
     flex-grow: 1;
-    max-width: 473px;
-    margin: auto;
-    padding-left: 40px;
-
+    z-index: 2;
+    height: 100%;
+    border-radius: 40px 0px 0px 40px;
+    min-width: 473px;
+    background-color: white;
+    
     display: flex;
-    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     @media (max-width: 768px) { /* 모바일 */
-      padding-left: 0;
+      padding-left: 20px;
+      padding-right: 20px;
     }
+`
+
+const LoginDetailItemWrapper = styled.div`
+  padding-left: 40px;
+  padding-right: 40px;
+  max-width: 473px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-itmes: center;
+  justify-content: center;
 `
