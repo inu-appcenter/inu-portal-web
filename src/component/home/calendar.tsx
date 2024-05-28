cebbba98a3d45d6dbe0183a4a3001983ee54076d
seokahi <skh9797@naver.com>
2024-05-28 14:56:16 +0900

    fix: 배포 에러 수정

--- src/component/home/calendar.tsx
+++ src/component/home/calendar.tsx
@@ -1,65 +1,4 @@
-// import { useRef, useEffect, useState } from "react";
-// import dayGridPlugin from "@fullcalendar/daygrid";
-// import FullCalendar from "@fullcalendar/react";
-// import { EventInput } from "@fullcalendar/core/index.js";
-
-// import getCalendar from "../../utils/getCalendar";
-// import "./calendar.css";
-
-// export default function Calendarbar() {
-//   const calendarRef = useRef<FullCalendar>(null);
-//   const [events, setEvents] = useState<EventInput[]>([]);
-//   const [month, setMonth] = useState<number>(0);
-//   const [year, setYear] = useState<number>(0);
-
-//   useEffect(() => {
-//     getCurrentMonth();
-//   }, []);
-
-//   useEffect(() => {
-//     fetchCalendarData();
-//   }, [year, month]);
-
-//   const fetchCalendarData = async () => {
-//     try {
-//       const response = await getCalendar(year, month);
-//       console.log(response, "return 값");
-//       setEvents(response.data);
-//     } catch (error) {
-//       console.error("학식 정보 조회 안됨");
-//     }
-//   };
-
-//   const getCurrentMonth = () => {
-//     const calendarApi = calendarRef.current?.getApi();
-//     if (calendarApi) {
-//       const currentDate = calendarApi.getDate();
-//       const year = currentDate.getFullYear();
-//       const month = currentDate.getMonth() + 1;
-//       setYear(year);
-//       setMonth(month);
-//       console.log("현재 month", month, year);
-//     }
-//   };
-
-//   const handleDateChange = () => {
-//     getCurrentMonth();
-//   };
-
-//   return (
-//     <FullCalendar
-//       ref={calendarRef}
-//       plugins={[dayGridPlugin]}
-//       initialView={"dayGridMonth"}
-//       headerToolbar={{ start: "title", end: "prev,next" }}
-//       eventTextColor="gray"
-//       datesSet={handleDateChange}
-//       events={events} 
-//     />
-//   );
-// }
-
-import React, { useRef, useEffect, useState } from "react";
+import  { useRef, useEffect, useState } from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import FullCalendar from "@fullcalendar/react";
 import { EventInput } from "@fullcalendar/core/index.js";
@@ -132,7 +71,6 @@ export default function Calendarbar() {
       {hoveredEvent && (
         <div className="event-tooltip">
           <p>{hoveredEvent}</p>
-          {/* 여기에 원하는 다른 이벤트 정보를 표시할 수 있습니다. */}
         </div>
       )}
     </div>
