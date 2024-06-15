3b5779c12dca02cb9349fe58ad03740f3546aa6f
2mingyu <tnt01010@naver.com>
2024-06-16 05:53:02 +0900

    refactor: Folders API

--- src/component/mypage/Scrap/ScrapFolder.tsx
+++ src/component/mypage/Scrap/ScrapFolder.tsx
@@ -1,18 +1,12 @@
-
-
 import styled from 'styled-components';
-
-import folderImg from "../../../resource/assets/file-img.svg"
+import folderImg from "../../../resource/assets/file-img.svg";
 import PlusImg from "../../../resource/assets/+.svg";
-import {  useState } from 'react';
-
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import CreateFolder from '../../../utils/postMakeFolder';
-
-import deleteFolder from '../../../utils/deletefolder';
+import { postFolders, deleteFolders } from '../../../utils/API/Folders';
 import { addFolder, removeFolder } from '../../../reducer/folderSlice';
 import MakeModal from './ScrapFolderModal';
-import deleteImg from "../../../resource/assets/deletebtn.svg"
+import deleteImg from "../../../resource/assets/deletebtn.svg";
 
 interface loginInfo {
     user: {
@@ -20,118 +14,123 @@ interface loginInfo {
     };
 }
 
-
 interface ScrapFolderInfoProps {
-    folderData:{ [key: number]: string };
+    folderData: { [key: number]: string };
     handleFolderClick: (folderId: number) => void;
-   setFolderData: (folder: { [key: number]: string }) => void;
-    setIsScrap:(status:boolean) => void;
-    setIsFolderScrap:(status:boolean) => void;
-  }
-  
-export default function ScrapFolder({folderData,handleFolderClick,setFolderData,setIsScrap,setIsFolderScrap}:ScrapFolderInfoProps) {
+    setFolderData: (folder: { [key: number]: string }) => void;
+    setIsScrap: (status: boolean) => void;
+    setIsFolderScrap: (status: boolean) => void;
+}
+
+export default function ScrapFolder({ folderData, handleFolderClick, setFolderData, setIsScrap, setIsFolderScrap }: ScrapFolderInfoProps) {
     const token = useSelector((state: loginInfo) => state.user.token);
     const [isOpenModal, setOpenModal] = useState<boolean>(false);
     const dispatch = useDispatch();
 
     const handleMakeFolder = () => {
-        console.log("ehlsrjsl");
+        console.log("폴더 생성");
         setOpenModal(true);
     };
 
     const closeModal = () => {
         setOpenModal(false);
     };
-    
+
     const handleFolderCreate = async (folderName: string) => {
         try {
-            const response = await CreateFolder(token, folderName);
-            const newFolderId = response.data;
-            setFolderData({ ...folderData, [newFolderId]: folderName });
-            dispatch(addFolder({[newFolderId]: folderName}));
+            const response = await postFolders(token, folderName);
+            if (response.status === 201) {
+                const newFolderId = response.body.data;
+                setFolderData({ ...folderData, [newFolderId]: folderName });
+                dispatch(addFolder({ [newFolderId]: folderName }));
+                console.log('폴더 생성 성공:', response.body);
+            } else {
+                console.error('폴더 생성 실패:', response.status);
+            }
         } catch (error) {
             console.error("폴더를 생성하지 못했습니다.", error);
         }
     };
- 
-   
 
-
-const handleFolderDelete = async (folderIdToDelete: number) => {
-    try {
-        await deleteFolder(token,folderIdToDelete);
-        const updatedData = { ...folderData };
-        delete updatedData[folderIdToDelete];
-        setFolderData(updatedData);
-        dispatch(removeFolder({ [folderIdToDelete]: true }));
-        setIsScrap(true);
-        setIsFolderScrap(false);
-    } catch (error) {
-        console.error("스크랩 폴더 삭제를 실패하였습니다.", error);
-    }
-};
+    const handleFolderDelete = async (folderIdToDelete: number) => {
+        try {
+            const response = await deleteFolders(token, folderIdToDelete);
+            if (response.status === 200) {
+                const updatedData = { ...folderData };
+                delete updatedData[folderIdToDelete];
+                setFolderData(updatedData);
+                dispatch(removeFolder({ [folderIdToDelete]: true }));
+                setIsScrap(true);
+                setIsFolderScrap(false);
+                console.log('폴더 삭제 성공:', response.body);
+            } else {
+                console.error('폴더 삭제 실패:', response.status);
+            }
+        } catch (error) {
+            console.error("스크랩 폴더 삭제를 실패하였습니다.", error);
+        }
+    };
 
     return (
         <>
             <Container>
                 {Object.entries(folderData).map(([folderId, folderName]) => (
                     <FolderWrapper key={folderId}>
-                        <Folder src={folderImg} alt=""  onClick={() => handleFolderClick(Number(folderId))}/>
+                        <Folder src={folderImg} alt="" onClick={() => handleFolderClick(Number(folderId))} />
                         <FolderNameInput>{folderName}</FolderNameInput>
                         <img className="delete" src={deleteImg} onClick={() => handleFolderDelete(Number(folderId))}></img>
                     </FolderWrapper>
                 ))}
                 <FolderAddWrapper>
-                <Plus src={PlusImg} onClick={handleMakeFolder}/>
+                    <Plus src={PlusImg} onClick={handleMakeFolder} />
                 </FolderAddWrapper>
                 {isOpenModal && <MakeModal closeModal={closeModal} onChange={handleFolderCreate} />}
             </Container>
-   </>
+        </>
     );
 }
 
 const Container = styled.div`
     display: flex;
     align-items: center;
-
-    flex-wrap:wrap;
+    flex-wrap: wrap;
     column-gap: 50px;
-    padding:19px 77px;
+    padding: 19px 77px;
     background: linear-gradient(#DBEBFF 10%, #FFFFFF);
-`
+`;
+
 const FolderWrapper = styled.div`
     display: flex;
     align-items: center;
     margin-top: 20px;
     position: relative;
     .delete {
-        border:none;
+        border: none;
         width: 11px;
         height: 11px;
         background-color: #4071B9;
         border-radius: 50%;
-        color:white;
+        color: white;
         line-height: 11px;
         position: absolute;
         left: 90%;
-        bottom:90%;
+        bottom: 90%;
     }
-
 `;
 
 const Folder = styled.img`
-position: relative;
+    position: relative;
 `;
 
 const FolderNameInput = styled.p`
     position: absolute;
     bottom: 10px;
-    margin-left:20px;
+    margin-left: 20px;
     width: 84px;
     height: 22px;
     border: none;
     border-radius: 5px;
-    background-color:  #FFFFFFB2;
+    background-color: #FFFFFFB2;
     text-align: center;
     font-size: 12px;
     font-weight: 700;
@@ -140,12 +139,13 @@ const FolderNameInput = styled.p`
 const Plus = styled.img`
     width: 30px;
     height: 30px;
-    color:gray;
-`
+    color: gray;
+`;
+
 const FolderAddWrapper = styled.div`
-       width: 128px;
-       height: 128px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-`
+    width: 128px;
+    height: 128px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
---
+++ src/utils/API/Folders.ts
@@ -0,0 +1,46 @@
+// utils/API/Folders.ts - 스크랩폴더 API
+import apiClient from './apiClient';
+
+// 스크랩폴더의 모든 게시글 가져오기
+export const getFoldersPosts = async (token: string, folderId: number, sort: string, page: number) => {
+  const apiURL = `https://portal.inuappcenter.kr/api/folders/${folderId}?sort=${sort}&page=${page}`;
+  return await apiClient(apiURL, 'GET', token);
+};
+
+// 스크랩폴더 삭제
+export const deleteFolders = async (token: string, folderId: number) => {
+  const apiURL = `https://portal.inuappcenter.kr/api/folders/${folderId}`;
+  return await apiClient(apiURL, 'DELETE', token);
+};
+
+// 회원의 모든 스크랩 폴더 가져오기
+export const getFolders = async (token: string) => {
+  const apiURL = `https://portal.inuappcenter.kr/api/folders`;
+  return await apiClient(apiURL, 'GET', token);
+};
+
+// 스크랩폴더 생성
+export const postFolders = async (token: string, foldername: string) => {
+  const apiURL = 'https://portal.inuappcenter.kr/api/folders';
+  const data = { name: foldername };
+  return await apiClient(apiURL, 'POST', token, data);
+};
+
+// 스크랩폴더에 게시글 담기
+export const postFoldersPosts = async (token: string, postId: number, selectedFolderIds: number[]) => {
+  const responses = await Promise.all(
+    selectedFolderIds.map(async (folderId) => {
+      const apiURL = `https://portal.inuappcenter.kr/api/folders/${folderId}/posts`;
+      const data = { postId: [postId] };
+      return await apiClient(apiURL, 'POST', token, data);
+    })
+  );
+  return responses;
+};
+
+// 스크랩폴더에서 게시글 빼기
+export const deleteFoldersPosts = async (token: string, postId: number, folderId: string) => {
+  const apiURL = `https://portal.inuappcenter.kr/api/folders/${folderId}/posts`;
+  const data = { postId: [postId] };
+  return await apiClient(apiURL, 'DELETE', token, data);
+};
