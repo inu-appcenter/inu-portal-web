63835e2da715d259d3e9b75f77cb63db78b08212
2mingyu <tnt01010@naver.com>
2024-06-23 05:47:46 +0900

    feat: mobile TipsPage

---
+++ src/mobile/pages/MobileTipsPage.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import TipsPageTitle from '../components/tips/TipsPageTitle';
+import CategorySelector from '../components/common/CategorySelector';
+import { useLocation } from 'react-router-dom';
+import ViewModeButtons from '../components/tips/ViewModeButtons';
+import TipsListContainer from '../containers/tips/TipsListContainer';
+
+export default function MobileTipsPage() {
+  const location = useLocation();
+  const [category, setCategory] = useState('전체');
+  const [viewMode, setViewMode] = useState<'grid' | 'list'>('list');
+  const [docType, setDocType] = useState('');  // TIPS 또는 NOTICE
+
+  useEffect(() => {
+    if (location.pathname.includes('/tips')) {
+      if (location.pathname.includes('/tips/notice')) {
+        setDocType('NOTICE')
+      }
+      else {
+        setDocType('TIPS');
+      }
+    }
+  }, [location.pathname]);
+
+  return (
+    <MobileTipsPageWrapper>
+      <TitleCategorySelectorWrapper>
+        <TipsPageTitle value={docType} />
+        <ViewModeButtonCategorySelectorWrapper>
+          <ViewModeButtons viewMode={viewMode} setViewMode={setViewMode} />
+          <CategorySelector value={category} onChange={setCategory} />
+        </ViewModeButtonCategorySelectorWrapper>
+      </TitleCategorySelectorWrapper>
+      <Wrapper>
+        <TipsListContainer viewMode={viewMode} docType={docType} category={category} />
+      </Wrapper>
+    </MobileTipsPageWrapper>
+  );
+}
+
+const MobileTipsPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  padding: 0 16px 0 16px;
+  height: 100%;
+  width: 100%;
+`;
+
+const TitleCategorySelectorWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ViewModeButtonCategorySelectorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+`
