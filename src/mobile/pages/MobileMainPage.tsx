c13eb3f1b9fa1860b3cba09d9990557ca23ae000
2mingyu <tnt01010@naver.com>
2024-06-29 14:31:41 +0900

    feat: Mobile Intro

--- src/mobile/pages/MobileMainPage.tsx
+++ src/mobile/pages/MobileMainPage.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
-import MobileNav from '../containers/common/MobileNav';
+import MobileIntroPage from './MobileIntroPage';
 import MobileHeader from '../containers/common/MobileHeader';
+import MobileNav from '../containers/common/MobileNav';
 import MobileHomePage from './MobileHomePage';
 import MobileTipsPage from './MobileTipsPage';
 // import MobileSavePage from './MobileSavePage';
@@ -20,6 +21,7 @@ const Page = styled.div<{ $active: boolean }>`
 `;
 
 export default function MobileMainPage() {
+  const [showIntro, setShowIntro] = useState(true);
   const location = useLocation();
   const [activePage, setActivePage] = useState('/m');
   const [pagesLoaded, setPagesLoaded] = useState<Record<string, boolean>>({
@@ -32,6 +34,25 @@ export default function MobileMainPage() {
   });
 
   const previousPages = usePreviousPage();
+
+  useEffect(() => {
+    // 세션 스토리지에서 'introShown' 키 확인
+    const introShown = sessionStorage.getItem('introShown');
+    if (introShown) {
+      // 'introShown' 키가 있으면, 새로고침으로 간주하고 IntroPage를 보여주지 않음
+      setShowIntro(false);
+    }
+    else {
+      // 처음 로드시, 'introShown' 키를 세션 스토리지에 추가
+      sessionStorage.setItem('introShown', 'true');
+      
+      const timer = setTimeout(() => {
+        setShowIntro(false);
+      }, 3000);
+      
+      return () => clearTimeout(timer);
+    }
+  }, [])
 
   useEffect(() => {
     const path = location.pathname.split('/')[2] || 'home';
@@ -48,6 +69,7 @@ export default function MobileMainPage() {
 
   return (
     <MobileMainPageWrapper>
+      {showIntro ? (<MobileIntroPage/>) : (<></>)}
       {!isLoginPage && (
         <header>
           <MobileHeader />
