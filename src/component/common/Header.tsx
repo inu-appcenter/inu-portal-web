8199af9cf03e20d8acdece96bd8b729657d08e23
2mingyu <tnt01010@naver.com>
2024-07-02 03:10:18 +0900

    feat: Header에 모바일로 이동 추가

--- src/component/common/Header.tsx
+++ src/component/common/Header.tsx
@@ -1,67 +1,56 @@
 import styled from 'styled-components';
 
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logoutUser } from '../../reducer/userSlice';
 
 export default function Headerbar() {
-    const nickname = useSelector((state: any) => state.user.nickname);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('refreshToken');
-        dispatch(logoutUser());
-        navigate('/');
-    };
+  const nickname = useSelector((state: any) => state.user.nickname);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    dispatch(logoutUser());
+    navigate('/');
+  };
 
-    return (
-        <React.Fragment>
-            <Header>
-                {nickname ? (
-                    <> 
-                        <LoginInfo>{nickname}</LoginInfo>
-                        <span> </span>
-                        <span onClick={handleLogout} className='login'>로그아웃</span>
-                       
-                    </>
-                ) : (
-                    <Link to='/login' className='login'>로그인</Link>
-                )} 
-            </Header>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Header>
+        <NavItem onClick={() => navigate('/m/home')}>모바일</NavItem>
+        {nickname ? (
+          <div> 
+            <NavItem>{nickname}</NavItem>
+            <NavItem onClick={handleLogout}>로그아웃</NavItem>
+          </div>
+        ) : (
+          <NavItem onClick={() => navigate('/login')} >로그인</NavItem>
+        )} 
+      </Header>
+    </React.Fragment>
+  )
 }
 
 const Header = styled.header`
-    position: relative;
-    left: 50%;
-    transform: translateX(-50%);
-    display: flex;
-    top:0;
-    height: 30px;
-    background: linear-gradient(to right, #9CAFE2 0%, #AAC9EE 100%);
-    justify-content: flex-end;
-    align-items: center;
-
-    .login {
-        font-size: 17px;
-        font-weight: 300;
-        line-height: 20px;
-        color: #FFFFFF;
-        text-decoration: none;
-        cursor: url('/pointers/cursor-pointer.svg'), pointer;
-        margin-right: 20px;
-    }
-
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  top:0;
+  justify-content: space-between;
+  align-items: center;
+  height: 30px;
+  background: linear-gradient(to right, #9CAFE2 0%, #AAC9EE 100%);
 `;
 
-const LoginInfo = styled.span`
-    font-size: 17px;
-    font-weight: 300;
-    line-height: 20px;
-    color: #FFFFFF;
-    text-decoration: none;
-    margin-right: 10px;
-`
+const NavItem = styled.span`
+  font-size: 17px;
+  font-weight: 300;
+  line-height: 20px;
+  color: #FFFFFF;
+  text-decoration: none;
+  cursor: url('/pointers/cursor-pointer.svg'), pointer;
+  margin: 0 20px;
+`;
