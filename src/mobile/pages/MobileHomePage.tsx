01f17a52c8c356c6d6ccacbf95e7dba9a1c4ac3d
seokahi <skh9797@naver.com>
2024-07-02 23:22:37 +0900

    add: 컴포넌트들 추가

--- src/mobile/pages/MobileHomePage.tsx
+++ src/mobile/pages/MobileHomePage.tsx
@@ -1,22 +1,41 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import WeatherForm from '../containers/home/Weather';
+import SerachForm from '../containers/home/SerachForm';
+import CategoryForm from '../containers/home/Category';
+import TipForm from '../containers/home/Tips';
+import AiForm from '../containers/home/Ai';
+import NoticeForm from '../containers/home/Notice';
 
 export default function MobileHomePage() {
-  const navigate = useNavigate();
 
   return(
     <MobileHomePageWrapper>
-      <button onClick={() => navigate('/m/home/tips')}>tips</button>
-      <button onClick={() => navigate('/m/home/tips/notice')}>notice</button>
+      <WeatherForm/>
+      <ContainerWrapper>
+        <SerachForm/>
+        <CategoryForm/>
+        <TipForm/>
+        <AiForm/>
+        <NoticeForm/>
+      </ContainerWrapper>
+
     </MobileHomePageWrapper>
   );
 }
 
 const MobileHomePageWrapper = styled.div`
-  display: flex;
+  /* display: flex;
   flex-direction: column;
   align-items: center;
   gap: 8px;
   margin: 16px 32px 0 32px;
-  height: 92%;
+  height: 92%; */
+  display: flex;
+  flex-direction: column;
+  margin-top: 170px;
+  width: 100%;
 `;
+
+const ContainerWrapper = styled.div`
+  margin:0 25px;
+`
