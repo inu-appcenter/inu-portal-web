39e03802ec639831dfe49b08baf3cffc531a8398
2mingyu <tnt01010@naver.com>
2024-07-15 19:42:21 +0900

    style: MobileSavePage ManageFolder

--- src/mobile/pages/MobileSavePage.tsx
+++ src/mobile/pages/MobileSavePage.tsx
@@ -41,8 +41,9 @@ export default function MobileSavePage() {
     setSelectedFolder(null);
   };
 
-  const handleFolderManaged = () => {
-    fetchFolders();
+  const handleFolderManaged = async () => {
+    await fetchFolders();
+    setSelectedFolder(null);
   };
 
   const handleSelectFolder = (folder: Folder) => {
