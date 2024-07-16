8924f6f05717c7110d616bb257fa48f4ecd63bc9
seokahi <skh9797@naver.com>
2024-07-16 11:58:26 +0900

    feat: 마이페이지 string 추가

---
+++ src/resource/string/m-mypage.tsx
@@ -0,0 +1,17 @@
+import writeImg from "../assets/mobile/mypage/write.png"
+import scrapImg from "../assets/mobile/mypage/scrap.png"
+import profileImg from "../assets/mobile/mypage/profile.png"
+import logoutImg from "../assets/mobile/mypage/logout.svg"
+import removeImg from "../assets/mobile/mypage/remove.png"
+export const MyPageActive = [
+    {title:'내가 쓴 글',image: `${writeImg}`},
+    {title:'좋아요한 글',image: `${writeImg}`},
+    {title:'작성한 댓글',image: `${writeImg}`},]
+
+
+export const MyPageCategory = [
+    {title:'프로필 편집',image: `${profileImg}`},
+    {title:'스크랩',image: `${scrapImg}`},
+    {title:'로그아웃',image: `${logoutImg}`},
+    {title:'회원탈퇴',image: `${removeImg}`}
+    ,]
