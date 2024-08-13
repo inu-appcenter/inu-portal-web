ba50632e06aaf5997d552896402f904337169070
2mingyu <tnt01010@naver.com>
2024-08-13 21:37:47 +0900

    feat: MobileAiIntro (드래그 미완)

---
+++ src/mobile/containers/ai/MobileAiInput.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function MobileAiInput() {
+  return(
+    <MobileAiInputWrapper>
+      MobileAiInput
+    </MobileAiInputWrapper>
+  );
+}
+
+const MobileAiInputWrapper = styled.div`
+  
+`
