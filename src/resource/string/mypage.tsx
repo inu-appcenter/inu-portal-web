9f35710388bcab32c6ae76d803a95e2a7bfd1446
2mingyu <tnt01010@naver.com>
2024-08-28 14:36:35 +0900

    fix: 웹 마이페이지 비밀번호 변경 탭 삭제

--- src/resource/string/mypage.tsx
+++ src/resource/string/mypage.tsx
@@ -1,19 +1,24 @@
 export const MypageList = [
-    {
-        title:'스크랩'
-    },
-    {
-        title:'내 활동'
-    },
-    {
-        title:'비밀번호 변경'
-    },
-    {
-        title:'개인정보 수정'
-    },
-    {
-        title:'회원탈퇴'
-    }
-]
+  {
+    title: "스크랩",
+  },
+  {
+    title: "내 활동",
+  },
+  // {
+  //     title:'비밀번호 변경'
+  // },
+  {
+    title: "개인정보 수정",
+  },
+  {
+    title: "회원탈퇴",
+  },
+];
 
-export const MyPageCategory =["스크랩", "내 활동","비밀번호 변경","개인정보 수정","회원탈퇴"];
+export const MyPageCategory = [
+  "스크랩",
+  "내 활동", // "비밀번호 변경",
+  "개인정보 수정",
+  "회원탈퇴",
+];
