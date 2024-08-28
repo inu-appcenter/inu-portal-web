f5369d5d9f61a07225523978eb9e27f2e3e5496e
2mingyu <tnt01010@naver.com>
2024-08-28 15:24:30 +0900

    refactor: login

--- src/component/login/FindPasswordButton.tsx
+++
@@ -1,9 +0,0 @@
-interface FindPasswordButtonProps {
-  onButtonClick: () => void;
-}
-
-export default function FindPasswordButton ({ onButtonClick }: FindPasswordButtonProps) {
-  return (
-    <button onClick={onButtonClick}>비밀번호 찾기</button>
-  )
-}
