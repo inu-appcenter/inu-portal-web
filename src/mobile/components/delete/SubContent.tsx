24f05886cf0eee5edd8fa52de50c9f70de0e85e6
seokahi <skh9797@naver.com>
2024-08-30 13:43:42 +0900

    feat: 수정사항 변경

--- src/mobile/components/delete/SubContent.tsx
+++ src/mobile/components/delete/SubContent.tsx
@@ -8,7 +8,6 @@ export default function SubContent() {
             {deleteInfo.map((content, index) => (
                 <div key={index}>
                     <h1>{content.title}</h1>
-                    <h3>{content.subtitle}</h3>
                 </div>
             ))}
         </SubContentWrapper>
@@ -19,12 +18,16 @@ const SubContentWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin:30px 33px;
+    margin:30px 35px;
     gap:16px;
+    box-sizing: border-box;
     div {
+        width: 100%;
         background-color: #F3F7FE;
         border-radius: 10px;
         padding:10px 5px;
+        text-align: left;
+        padding:10px;
         h1 {
         font-family: Roboto;
         font-size: 13px;
--- src/resource/string/m-mypage.tsx
+++ src/resource/string/m-mypage.tsx
@@ -1,12 +1,14 @@
-import writeImg from "../assets/mobile/mypage/write.svg"
+import writeImg from "../assets/post-img.svg"
+import likeImg from "../assets/like-img.svg"
+import commentImg from "../assets/comment-img.svg"
 import scrapImg from "../assets/mobile/mypage/scrap.svg"
 import profileImg from "../assets/mobile/mypage/profile.svg"
 import logoutImg from "../assets/mobile/mypage/logout.svg"
 import removeImg from "../assets/mobile/mypage/remove.svg"
 export const MyPageActive = [
     {title:'내가 쓴 글',image: `${writeImg}`},
-    {title:'좋아요 한 글',image: `${writeImg}`},
-    {title:'작성한 댓글',image: `${writeImg}`},]
+    {title:'좋아요 한 글',image: `${likeImg}`},
+    {title:'작성한 댓글',image: `${commentImg}`},]
 
 
 export const MyPageCategory = [
