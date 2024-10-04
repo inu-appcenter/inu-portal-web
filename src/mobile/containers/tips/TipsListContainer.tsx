91161c4b231bc62c581260a52f378d33af8decfa
2mingyu <tnt01010@naver.com>
2024-10-05 04:08:42 +0900

    feat: 모바일 TipsListContainer 인피니티 스크롤 수정

--- src/mobile/containers/tips/TipsListContainer.tsx
+++ src/mobile/containers/tips/TipsListContainer.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import styled from "styled-components";
 import TipsCard from "../../components/tips/TipsCard";
-import { getPosts } from "../../../utils/API/Posts";
+import { getPostsMobile } from "../../../utils/API/Posts";
 import { getNotices } from "../../../utils/API/Notices";
 import { search } from "../../../utils/API/Search";
 
@@ -18,180 +19,88 @@ export default function TipsListContainer({
   category,
   query,
 }: TipsListContainerProps) {
-  const [posts, setPosts] = useState<(Post | { page: number })[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [lastPostId, setLastPostId] = useState<number | undefined>(undefined);
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const containerRef = useRef<HTMLDivElement>(null);
+  const [hasMore, setHasMore] = useState(true);
 
-  // 데이터 가져오기 함수
-  const fetchData = useCallback(
-    async (pageToLoad: number) => {
-      setLoading(true);
+  useEffect(() => {
+    setPage(1);
+    setLastPostId(undefined);
+    setHasMore(true);
+    setPosts([]);
+  }, [query, docType, setPosts, category]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(docType, page, category, lastPostId);
       try {
         let response;
         if (docType === "TIPS") {
-          response = await getPosts(category, "date", pageToLoad.toString());
+          response = await getPostsMobile(lastPostId, category);
         } else if (docType === "NOTICE") {
-          response = await getNotices(category, "date", pageToLoad.toString());
+          response = await getNotices(category, "date", page.toString());
         } else if (docType === "SEARCH" && query) {
-          response = await search(query, "date", pageToLoad.toString());
+          response = await search(query, "date", page.toString());
         }
-
         if (response && response.status === 200) {
-          console.log("정보 갖고옴");
-          const newPosts =
-            response.body.data.posts || response.body.data.notices;
-          setPosts((prev) =>
-            pageToLoad === 1
-              ? [{ page: pageToLoad }, ...newPosts]
-              : [...prev, { page: pageToLoad }, ...newPosts]
-          );
-          setTotalPages(response.body.data.pages);
+          let newPosts;
+          if (docType === "TIPS") {
+            newPosts = response.body.data;
+          } else if (docType === "NOTICE") {
+            newPosts = response.body.data.notices;
+          } else if (docType === "SEARCH") {
+            newPosts = response.body.data.posts;
+          }
+          if (newPosts && newPosts.length > 0) {
+            setPosts((prev) =>
+              posts.length === 0 ? newPosts : [...prev, ...newPosts]
+            );
+          } else {
+            setHasMore(false);
+          }
+        } else {
+          setHasMore(false);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
-      }
-      setLoading(false);
-    },
-    [category, docType, query]
-  );
-
-  const fetchInitialData = useCallback(async () => {
-    setLoading(true);
-    try {
-      let responsePage1;
-      let responsePage2;
-
-      if (docType === "TIPS") {
-        responsePage1 = await getPosts(category, "date", "1");
-        responsePage2 = await getPosts(category, "date", "2");
-      } else if (docType === "NOTICE") {
-        responsePage1 = await getNotices(category, "date", "1");
-        responsePage2 = await getNotices(category, "date", "2");
-      } else if (docType === "SEARCH" && query) {
-        responsePage1 = await search(query, "date", "1");
-        responsePage2 = await search(query, "date", "2");
-      }
-
-      if (responsePage1?.status === 200 && responsePage2?.status === 200) {
-        const newPostsPage1 =
-          responsePage1.body.data.posts || responsePage1.body.data.notices;
-        const newPostsPage2 =
-          responsePage2.body.data.posts || responsePage2.body.data.notices;
-        setPosts([
-          { page: 1 },
-          ...newPostsPage1,
-          { page: 2 },
-          ...newPostsPage2,
-        ]);
-        setTotalPages(responsePage1.body.data.pages);
-      }
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-    setLoading(false);
-  }, [category, docType, query]);
-
-  // 데이터 더 가져오기 함수
-  const loadMoreData = useCallback(async () => {
-    if (page <= totalPages) {
-      await fetchData(page);
-      setPage((prev) => prev + 1);
-    }
-  }, [fetchData, page, totalPages]);
-
-  // 카테고리 또는 검색어 변경 시 데이터 리셋 및 첫 페이지 로드
-  useEffect(() => {
-    setPage(3); // 초기 로드 시 두 페이지를 로드하기 때문에 페이지 번호를 3으로 설정합니다.
-    fetchInitialData();
-    // 스크롤 맨 위로 올리기
-    if (containerRef.current) {
-      containerRef.current.scrollTop = 0;
-    }
-  }, [category, fetchInitialData, query]);
-
-  // 스크롤 핸들러
-  const handleScroll = () => {
-    if (containerRef.current) {
-      const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
-      if (scrollTop + clientHeight >= scrollHeight - 5 && !loading) {
-        loadMoreData();
-      }
-    }
-  };
-
-  // 스크롤 이벤트 리스너 추가 및 제거
-  useEffect(() => {
-    const currentRef = containerRef.current;
-    if (currentRef) {
-      currentRef.addEventListener("scroll", handleScroll);
-    }
-    return () => {
-      if (currentRef) {
-        currentRef.removeEventListener("scroll", handleScroll);
+        setHasMore(false);
       }
     };
-  }, [handleScroll]);
+    fetchData();
+  }, [category, docType, query, page, lastPostId]);
 
-  // 페이지별로 그룹화된 데이터를 렌더링
-  const renderPosts = () => {
-    const groupedPosts: JSX.Element[] = [];
-    let currentPage = 1;
-    let pagePosts: (Post | { page: number })[] = [];
-
-    posts.forEach((post, index) => {
-      if ("page" in post) {
-        if (pagePosts.length > 0) {
-          groupedPosts.push(
-            <PageGroup key={`page-${currentPage}`}>
-              <PageMarker>Page {currentPage}</PageMarker>
-              <TipsCardWrapper $viewMode={viewMode}>
-                {pagePosts.map((p, i) => (
-                  <TipsCard
-                    key={`post-${index}-${i}`}
-                    post={p as Post}
-                    viewMode={viewMode}
-                    docType={docType}
-                  />
-                ))}
-              </TipsCardWrapper>
-            </PageGroup>
-          );
-          pagePosts = [];
-        }
-        currentPage = post.page;
-      } else {
-        pagePosts.push(post);
-      }
-    });
-
-    if (pagePosts.length > 0) {
-      groupedPosts.push(
-        <PageGroup key={`page-${currentPage}`}>
-          <PageMarker>Page {currentPage}</PageMarker>
-          <TipsCardWrapper $viewMode={viewMode}>
-            {pagePosts.map((p, i) => (
-              <TipsCard
-                key={`post-${currentPage}-${i}`}
-                post={p as Post}
-                viewMode={viewMode}
-                docType={docType}
-              />
-            ))}
-          </TipsCardWrapper>
-        </PageGroup>
-      );
+  const setNext = () => {
+    if (docType === "TIPS") {
+      setLastPostId(Number(posts[posts.length - 1]?.id));
+    } else {
+      setPage((page) => page + 1);
     }
-
-    return groupedPosts;
   };
 
   return (
-    <TipsListContainerWrapper $docType={docType} ref={containerRef}>
-      {renderPosts()}
-      {loading && <Loader>Loading...</Loader>}
-      {!loading && page > totalPages && <EndMarker>End of Content</EndMarker>}
+    <TipsListContainerWrapper id="scrollableDiv" $docType={docType}>
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={setNext}
+        hasMore={hasMore}
+        loader={<h4 style={{ textAlign: "center" }}>Loading...</h4>}
+        endMessage={
+          <h4 style={{ textAlign: "center" }}>더 이상 게시물이 없습니다.</h4>
+        }
+        scrollableTarget="scrollableDiv"
+      >
+        <TipsCardWrapper $viewMode={viewMode}>
+          {posts.map((p, i) => (
+            <TipsCard
+              key={i}
+              post={p as Post}
+              viewMode={viewMode}
+              docType={docType}
+            />
+          ))}
+        </TipsCardWrapper>
+      </InfiniteScroll>
     </TipsListContainerWrapper>
   );
 }
@@ -208,10 +117,6 @@ const TipsListContainerWrapper = styled.div<{ $docType: string }>`
   overflow-y: auto;
 `;
 
-const PageGroup = styled.div`
-  margin-bottom: 16px;
-`;
-
 const TipsCardWrapper = styled.div<{ $viewMode: "grid" | "list" }>`
   display: ${({ $viewMode }) => ($viewMode === "grid" ? "grid" : "flex")};
   flex-direction: ${({ $viewMode }) =>
@@ -220,26 +125,4 @@ const TipsCardWrapper = styled.div<{ $viewMode: "grid" | "list" }>`
   width: 100%;
   grid-template-columns: ${({ $viewMode }) =>
     $viewMode === "grid" ? "repeat(2, 1fr)" : "unset"};
-`;
-
-const Loader = styled.div`
-  width: 100%;
-  padding: 4px 0;
-  text-align: center;
-  font-weight: bold;
-`;
-
-const PageMarker = styled.div`
-  display: none; // 개발용
-  width: 100%;
-  text-align: center;
-  font-weight: bold;
-  padding: 4px 0;
-`;
-
-const EndMarker = styled.div`
-  width: 100%;
-  text-align: center;
-  font-weight: bold;
-  padding: 4px 0;
 `;
