db590f2e5b77a114be0b497cfa1eec061726dc72
2mingyu <tnt01010@naver.com>
2024-10-05 04:40:15 +0900

    fix: 모바일 검색 검색어 두글자 제한

--- src/mobile/containers/home/SerachForm.tsx
+++ src/mobile/containers/home/SerachForm.tsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useState } from "react";
+import styled from "styled-components";
 import searchImg from "../../../resource/assets/mobile/home/input.svg";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function SerachForm() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
 
   const handleSearch = () => {
-    if (!query.trim()) {
-      alert('검색어는 두 글자 이상이어야 합니다.');
+    if (query.trim().length < 2) {
+      alert("검색어는 두 글자 이상이어야 합니다.");
       return;
     }
     navigate(`/m/home/tips/search?query=${query}`);
   };
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSearch();
     }
   };
@@ -24,9 +24,9 @@ export default function SerachForm() {
   return (
     <SearchFormWrapper>
       <div>
-        <input 
-          type="text" 
-          placeholder='검색어를 입력하세요.' 
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           onKeyPress={handleKeyPress}
@@ -42,16 +42,16 @@ const SearchFormWrapper = styled.div`
   div {
     box-sizing: border-box;
     border-radius: 10px;
-    box-shadow: 0px 2px 8px 0px #0000001A;
+    box-shadow: 0px 2px 8px 0px #0000001a;
     width: 100%;
     background-color: white;
     display: flex;
     justify-content: space-between;
-    padding:8px 17px;
+    padding: 8px 17px;
     input {
       border: none;
       font-size: 14px;
-      color:#888888;
+      color: #888888;
       font-weight: 500;
       flex-grow: 1;
     }
