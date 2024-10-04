e1e1505d7574a36fc1d4ddbab32aa61a23d61beb
2mingyu <tnt01010@naver.com>
2024-10-05 04:37:56 +0900

    fix: 모바일 스크랩, 좋아요 고치기

--- src/mobile/components/postdetail/util/m.postscrap.tsx
+++ src/mobile/components/postdetail/util/m.postscrap.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import scrapEmptyImg from '../../../../resource/assets/scrap.svg'
-import scrapFilledImg from '../../../../resource/assets/scrap-filled-img.svg';
-import { handlePostScrap } from '../../../../utils/API/Posts';
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import scrapEmptyImg from "../../../../resource/assets/scrap.svg";
+import scrapFilledImg from "../../../../resource/assets/scrap-filled-img.svg";
+import { handlePostScrap } from "../../../../utils/API/Posts";
 
 interface PostScrapProps {
   id: string;
@@ -10,43 +10,53 @@ interface PostScrapProps {
   isScrapedProp: boolean;
 }
 
-export default function PostScrap({ id, scrap, isScrapedProp }: PostScrapProps) {
+export default function PostScrap({
+  id,
+  scrap,
+  isScrapedProp,
+}: PostScrapProps) {
   const [scraps, setScraps] = useState(scrap);
   const [isScraped, setIsScraped] = useState(isScrapedProp);
   const token = useSelector((state: any) => state.user.token);
+
+  useEffect(() => {
+    setIsScraped(isScrapedProp);
+  }, [id]);
+
   const handleScrapClick = async () => {
-    if (id === undefined) {
-      console.error('ID is undefined');
+    if (!id) {
+      console.error("ID is undefined");
       return;
     }
     if (token) {
       try {
         const result = await handlePostScrap(token, id);
-        setIsScraped(!isScraped);
         if (result.body.data === +1) {
           setScraps(scraps + 1);
-          alert('스크랩 성공');
+          setIsScraped(true);
+          alert("스크랩 성공");
         } else {
           setScraps(scraps - 1);
-          alert('스크랩 취소');
+          setIsScraped(false);
+          alert("스크랩 취소");
         }
       } catch (error) {
-        console.error('스크랩 처리 에러', error);
-        alert('스크랩 처리 에러');
+        console.error("스크랩 처리 에러", error);
+        alert("스크랩 처리 에러");
       }
     } else {
-      alert('로그인 필요');
+      alert("로그인 필요");
     }
   };
 
   return (
-    <span className='scrapContainer'>
-    
-        <img onClick={handleScrapClick}
-          className='UtilityImg'
-          src={isScraped ? scrapFilledImg : scrapEmptyImg}
-          alt='scrapImg'
-        />
+    <span className="scrapContainer">
+      <img
+        onClick={handleScrapClick}
+        className="UtilityImg"
+        src={isScraped ? scrapFilledImg : scrapEmptyImg}
+        alt="scrapImg"
+      />
     </span>
   );
-};
+}
