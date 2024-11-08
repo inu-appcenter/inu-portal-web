ebb3ffbaeeb7ea2dd06f63eb5be30c0a3b9acfc8
2mingyu <tnt01010@naver.com>
2024-11-08 23:59:32 +0900

    feat: tsconfigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['icon.svg'],
-      manifest: false
-    })
-  ]
+      registerType: "autoUpdate",
+      includeAssets: ["icon.svg"],
+      manifest: false,
+    }),
+    tsconfigPaths(),
+  ],
 });
