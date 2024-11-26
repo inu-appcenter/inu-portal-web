86565d52cf25d33e72140360c45192a1bb4d472d
2mingyu <tnt01010@naver.com>
2024-11-27 00:40:50 +0900

    feat: 모바일 헤더 닉네임 표시 방식

--- src/mobile/components/common/ProfileNickname.tsx
+++ src/mobile/components/common/ProfileNickname.tsx
@@ -8,13 +8,17 @@ export default function ProfileNickname({ nickname }: ProfileNicknameProps) {
   return <ProfileNicknameWrapper>{nickname}</ProfileNicknameWrapper>;
 }
 
-const ProfileNicknameWrapper = styled.button`
-  width: 56px;
+const ProfileNicknameWrapper = styled.div`
+  min-width: 56px;
+  max-width: 100px;
   height: 24px;
   border-radius: 8px;
   border: none;
   background-color: white;
   display: flex;
   align-items: center;
-  justify-content: center;
+  overflow: hidden; /* 컨테이너 크기를 벗어나는 텍스트 숨김 */
+  padding: 0 4px;
+  font-size: 14px;
+  color: black;
 `;
