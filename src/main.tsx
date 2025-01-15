fd518e2c61f3a85cb15c6deb4c24ec2552758bdc
2mingyu <tnt01010@naver.com>
2025-01-15 15:17:15 +0900

    feat: Intersection Observer

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,11 @@
+import "intersection-observer";
 import ReactDOM from "react-dom/client";
 import App from "App";
-// import { Provider } from "react-redux";
-// import { configureStore } from "@reduxjs/toolkit";
-// import userSlice from "./reducer/userSlice";
 import CommonStyles from "resources/styles/CommonStyles";
-// import folderSlice from "./reducer/folderSlice";
-// import folderIdSlice from "./reducer/folderId";
-
-// const store = configureStore({
-//   reducer: {
-//     user: userSlice,
-//     folder: folderSlice,
-//     folderId: folderIdSlice,
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false,
-//     }),
-// });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <Provider store={store}>
   <>
     <CommonStyles />
     <App />
   </>
-  // </Provider>
 );
