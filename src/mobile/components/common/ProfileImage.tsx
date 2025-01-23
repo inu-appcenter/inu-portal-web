1e9984dc7738831e1943e3f6ad3d74f033388fd7
2mingyu <tnt01010@naver.com>
2025-01-23 19:20:36 +0900

    fix: 모바일 헤더 프로필 클릭 시 url 고치기

--- src/mobile/components/common/ProfileImage.tsx
+++ src/mobile/components/common/ProfileImage.tsx
@@ -1,19 +1,21 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import useAppStateStore from "stores/useAppStateStore";
 
 interface ProfileImageProps {
   fireId: number;
 }
 
 export default function ProfileImage({ fireId }: ProfileImageProps) {
-  const navvigate = useNavigate();
+  const navigate = useNavigate();
+  const { isAppUrl } = useAppStateStore();
 
   return (
     <ProfileImg
       src={`https://portal.inuappcenter.kr/api/images/${fireId}`}
       alt={`Profile Image`}
       onClick={() => {
-        navvigate("/m/mypage");
+        navigate(`${isAppUrl}/mypage`);
       }}
     />
   );
