262cdb43c3978edc7e7e83a7c21cd412e5df36da
2mingyu <tnt01010@naver.com>
2025-01-27 00:55:31 +0900

    feat: useMobileNavigate 수정

--- src/hooks/useMobileNavigate.ts
+++ src/hooks/useMobileNavigate.ts
@@ -1,13 +1,24 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import useAppStateStore from "stores/useAppStateStore";
 
 export default function useMobileNavigate() {
   const navigate = useNavigate();
+  const location = useLocation();
   const { isAppUrl } = useAppStateStore();
 
   const mobileNavigate = (path: string) => {
+    const currentPathname = location.pathname;
+    const currentSearch = location.search;
+    const [newPathname, newSearch] = path.split("?");
+
     if (window.AndroidBridge && window.AndroidBridge.navigateTo) {
-      window.location.href = `${isAppUrl}${path}`;
+      if (currentPathname === newPathname) {
+        if (currentSearch !== `?${newSearch}`) {
+          navigate(`${isAppUrl}${path}`);
+        }
+      } else {
+        window.location.href = `${isAppUrl}${path}`;
+      }
     } else {
       navigate(`${isAppUrl}${path}`);
     }
