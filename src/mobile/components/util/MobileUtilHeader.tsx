4dd1da42ff30b7854e6045d2272dad3fd9e7fb09
2mingyu <tnt01010@naver.com>
2025-01-29 06:02:30 +0900

    design: 총학생회, 편의 페이지 헤더

--- src/mobile/components/util/MobileUtilHeader.tsx
+++ src/mobile/components/util/MobileUtilHeader.tsx
@@ -2,52 +2,50 @@ import useMobileNavigate from "hooks/useMobileNavigate";
 import styled from "styled-components";
 
 interface Props {
-    selectedType: string;
+  selectedType: string;
 }
 
 const buttons = [
-    {type: "book", label: "책 벼룩시장"},
-    {type: "lost", label: "분실물"},
-    {type: "rental", label: "물품 대여"},
+  { type: "lost", label: "분실물 게시판" },
+  { type: "rental", label: "물품 대여" },
+  { type: "book", label: "벼룩시장" },
 ];
 
-export default function MobileUtilHeader({selectedType}: Props) {
-    const mobileNavigate = useMobileNavigate();
+export default function MobileUtilHeader({ selectedType }: Props) {
+  const mobileNavigate = useMobileNavigate();
 
-    return (
-        <MobileUtilHeaderWrapper>
-            {buttons.map((btn) => (
-                <button
-                    key={btn.type}
-                    onClick={() => mobileNavigate(`/home/util?type=${btn.type}`)}
-                    className={selectedType === btn.type ? "selected" : ""}
-                >
-                    {btn.label}
-                </button>
-            ))}
-        </MobileUtilHeaderWrapper>
-    );
+  return (
+    <MobileUtilHeaderWrapper>
+      {buttons.map((btn) => (
+        <button
+          key={btn.type}
+          onClick={() => mobileNavigate(`/home/util?type=${btn.type}`)}
+          className={selectedType === btn.type ? "selected" : ""}
+        >
+          {btn.label}
+        </button>
+      ))}
+    </MobileUtilHeaderWrapper>
+  );
 }
 
 const MobileUtilHeaderWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100%;
-    min-height: 42px;
-    border-bottom: 2px solid #e0e0e0;
-
-    button {
-        box-sizing: content-box;
-        height: 100%;
-        flex: 1;
-        background-color: transparent;
-        border: 0;
-        border-bottom: 2px solid transparent;
-        font-weight: 600;
-    }
-
-    .selected {
-        color: #4071b9;
-        border-bottom: 2px solid #4071b9;
-    }
+  display: flex;
+  align-items: center;
+  width: 100%;
+  min-height: 46px;
+  color: rgba(155, 155, 155, 1);
+  background: rgba(243, 247, 254, 1);
+  button {
+    box-sizing: content-box;
+    height: 100%;
+    flex: 1;
+    background-color: transparent;
+    border: 0;
+    font-weight: 600;
+  }
+  .selected {
+    background: linear-gradient(180deg, #6d98d7 0%, #0e4d9d 100%);
+    color: white;
+  }
 `;
