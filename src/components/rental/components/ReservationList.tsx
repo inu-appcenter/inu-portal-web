5659de72b8499464eb387dc069ad1f1cb4475f76
baebaej <hinara12@inu.ac.kr>
2025-02-01 21:13:35 +0900

    배포 오류 수정

--- src/components/rental/components/ReservationList.tsx
+++ src/components/rental/components/ReservationList.tsx
@@ -1,17 +1,20 @@
 import {useState, useEffect} from "react";
-import {getReservations} from "apis/rental";  // getReservations 함수 임포트
+import {getReservations, Reservation, ReservationContent} from "apis/rental";  // getReservations 함수 임포트
 import ReservationItem from "./ReservationItem";  // ReservationItem 컴포넌트 임포트
 import styled from "styled-components";
 
 const ReservationList = () => {
-    const [reservations, setReservations] = useState([]);
+    const [reservations, setReservations] = useState<ReservationContent[]>([]);  // ReservationContent 배열로 타입 설정
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchReservations = async () => {
             try {
-                const data = await getReservations(1);  // 첫 페이지 예약 목록 가져오기
-                setReservations(data.contents);
+                // @ts-ignore
+                const data: ApiResponse<Reservation> = await getReservations(1);  // 첫 페이지 예약 목록 가져오기
+                console.log(data);
+                setReservations(data.contents);  // 유효한 배열이면 상태 업데이트
+
             } catch (error) {
                 console.error("예약 목록을 가져오는 중 오류 발생:", error);
             } finally {
@@ -26,15 +29,12 @@ const ReservationList = () => {
         return <p>로딩 중...</p>;
     }
 
-    // @ts-ignore
     return (
         <ReservationListWrapper>
-            {reservations.length > 0 ? (
+            {reservations && (  // reservations 배열이 비어 있지 않은지 확인
                 reservations.map((reservation) => (
                     <ReservationItem key={reservation.itemId} reservation={reservation}/>
                 ))
-            ) : (
-                <p>예약 목록이 없습니다.</p>
             )}
         </ReservationListWrapper>
     );
@@ -43,6 +43,6 @@ const ReservationList = () => {
 const ReservationListWrapper = styled.div`
     width: 100%;
     height: 100%;
-`
+`;
 
 export default ReservationList;
