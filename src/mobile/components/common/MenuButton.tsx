0c451eacc7b6579a557f7f63df585e3b044bf98b
2mingyu <tnt01010@naver.com>
2025-02-02 12:07:41 +0900

    feat: 모바일 사이드바

--- src/mobile/components/common/MenuButton.tsx
+++ src/mobile/components/common/MenuButton.tsx
@@ -1,72 +1,100 @@
 import styled from "styled-components";
 import menuButtonImage from "resources/assets/mobile-common/menu-button.svg";
-import { useEffect, useRef, useState } from "react";
-import { navItems as originalNavBarList } from "resources/strings/navItems";
+import intipLogo from "resources/assets/intip-logo.svg";
+import closeBtn from "resources/assets/mobile-common/closebtn.svg";
+import { useEffect, useState } from "react";
+import SerachForm from "mobile/containers/home/SerachForm";
+import AiForm from "mobile/containers/home/Ai";
+
+import menuImg from "resources/assets/mobile-home/menu.svg";
+import noticeImg from "resources/assets/mobile-home/notice.svg";
+import TipImg from "resources/assets/mobile-home/tip.svg";
+import calendarImg from "resources/assets/mobile-home/calendar.svg";
+import councilImg from "resources/assets/mobile-home/council.svg";
+import mapImg from "resources/assets/mobile-home/map.svg";
+import clubImg from "resources/assets/mobile-home/club.svg";
+import utilImg from "resources/assets/mobile-home/util.svg";
+import useMobileNavigate from "hooks/useMobileNavigate";
+import { useLocation } from "react-router-dom";
+
+const categorys = [
+  { title: "메뉴", img: menuImg },
+  { title: "공지사항", img: noticeImg },
+  { title: "TIPS", img: TipImg },
+  { title: "학사일정", img: calendarImg },
+  { title: "총학생회", img: councilImg },
+  { title: "캠퍼스", img: mapImg },
+  { title: "동아리", img: clubImg },
+  { title: "편의", img: utilImg },
+];
 
 export default function MenuButton() {
   const [isVisible, setIsVisible] = useState(false);
-
-  const handleSubItemClick = (url: string) => {
-    window.open(url, "_blank");
-  };
-  const wrapperRef = useRef<HTMLDivElement>(null);
+  const location = useLocation();
+  const mobileNavigate = useMobileNavigate();
 
   useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        wrapperRef.current &&
-        !wrapperRef.current.contains(event.target as Node)
-      ) {
-        setIsVisible(false);
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [wrapperRef]);
+    setIsVisible(false);
+  }, [location.pathname]);
 
-  const College = originalNavBarList.find(
-    (item) => item.title === "학과 홈페이지"
-  );
+  const handleClick = (title: string) => {
+    if (title === "메뉴") {
+      mobileNavigate(`/home/menu`);
+    } else if (title === "공지사항") {
+      mobileNavigate(`/home/tips?type=notice`);
+    } else if (title === "TIPS") {
+      mobileNavigate(`/home/tips`);
+    } else if (title === "학사일정") {
+      mobileNavigate(`/home/calendar`);
+    } else if (title === "총학생회") {
+      mobileNavigate(`/home/council`);
+    } else if (title === "동아리") {
+      mobileNavigate(`/home/club`);
+    } else if (title === "캠퍼스") {
+      mobileNavigate(`/home/campus`);
+    } else if (title === "편의") {
+      mobileNavigate(`/home/util`);
+    } else {
+      mobileNavigate(`/home/${title}`);
+    }
+  };
+
   return (
     <>
       <MenuButtonImg
         src={menuButtonImage}
-        alt="MenuButtonImg"
-        onClick={() => setIsVisible(!isVisible)}
+        alt="사이드바"
+        onClick={() => setIsVisible(true)}
       />
-      {isVisible && College && College.child && (
-        <Sidebar ref={wrapperRef}>
-          <CloseButton onClick={() => setIsVisible(false)}>×</CloseButton>
-          <NavList>
-            {College.child.map((childItem, childIndex) => (
-              <NavSubSection className={childItem.title} key={childIndex}>
-                {childItem.title === "법학부" ? (
-                  <NavSubTitle>{childItem.title}</NavSubTitle>
-                ) : childItem.url ? (
-                  <NavItem onClick={() => handleSubItemClick(childItem.url)}>
-                    {childItem.title}
-                  </NavItem>
-                ) : (
-                  <>
-                    <NavSubTitle>{childItem.title}</NavSubTitle>
-                    {childItem.subItems &&
-                      childItem.subItems.map((subItem, subIndex) => (
-                        <NavItem
-                          key={subIndex}
-                          onClick={() => handleSubItemClick(subItem.url)}
-                        >
-                          {subItem.title}
-                        </NavItem>
-                      ))}
-                  </>
-                )}
-                <br />
-              </NavSubSection>
-            ))}
-          </NavList>
-        </Sidebar>
+      {isVisible && (
+        <>
+          <Background onClick={() => setIsVisible(false)} />
+          <Sidebar>
+            <div className="sidebar-header">
+              <img className="logo" src={intipLogo} alt="" />
+              <button onClick={() => setIsVisible(false)}>
+                <span>닫기</span>
+                <img src={closeBtn} alt="" />
+              </button>
+            </div>
+            <div className="sidebar-categories">
+              <div className="sf-wrapper">
+                <SerachForm />
+              </div>
+              {categorys.map((category, index) => (
+                <div
+                  key={index}
+                  className="sidebar-category"
+                  onClick={() => handleClick(category.title)}
+                >
+                  <img src={category.img} alt="카테고리 이미지" />
+                  <span>{`[${category.title}]`}</span>
+                </div>
+              ))}
+              <AiForm />
+            </div>
+          </Sidebar>
+        </>
       )}
     </>
   );
@@ -76,61 +104,69 @@ const MenuButtonImg = styled.img`
   height: 18px;
 `;
 
+const Background = styled.div`
+  position: fixed;
+  inset: 0;
+  background-color: rgba(255, 255, 255, 0.5);
+`;
+
 const Sidebar = styled.div`
   position: fixed;
+  top: 0;
   bottom: 0;
-  left: 0;
-  height: 70%;
-  width: 100%;
+  right: 0;
+  width: 320px;
   background-color: #fff;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   z-index: 1000;
   display: flex;
   flex-direction: column;
   overflow-y: auto;
-`;
 
-const CloseButton = styled.button`
-  align-self: flex-end;
-  background: none;
-  border: none;
-  font-size: 24px;
-  color: #888888;
-  cursor: pointer;
-  margin: 10px;
-`;
+  .sidebar-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 8px 16px;
+    background-color: rgba(238, 238, 238, 1);
 
-const NavList = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-  padding: 0;
-  margin: 20px 0 0 0;
-  margin: 0 10px;
-`;
+    .logo {
+      height: 32px;
+    }
 
-const NavSubSection = styled.div`
-  flex: 1 1 100px;
-  margin-left: 10px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-  min-width: 100px;
-`;
+    button {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      background-color: transparent;
+      border: none;
+      span {
+        font-size: 14px;
+        font-weight: 500;
+      }
+    }
+  }
+  .sidebar-categories {
+    display: flex;
+    flex-direction: column;
+    margin: 0 16px;
 
-const NavSubTitle = styled.div`
-  font-weight: bold;
-  margin-bottom: 10px;
-  color: #0e4d9d;
-`;
+    .sf-wrapper {
+      margin: 12px 0;
+    }
+    .sidebar-category {
+      display: flex;
+      align-items: center;
+      gap: 16px;
+      height: 60px;
+      border-bottom: 1px solid rgba(223, 223, 223, 1);
 
-const NavItem = styled.div`
-  color: #888888;
-  font-size: 15px;
-  cursor: pointer;
-  &:hover {
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-    width: fit-content;
-    padding: 0 3px 0 3px;
-    color: #000;
+      img {
+        width: 32px;
+      }
+
+      span {
+        font-weight: 600;
+      }
+    }
   }
 `;
