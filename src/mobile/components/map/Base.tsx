0148cee35e2e6f08b83601f36b2fbd28582b5634
2mingyu <tnt01010@naver.com>
2025-02-05 03:42:15 +0900

    fix: 배포 오류 해결(Base.tsx)

--- src/mobile/components/map/Base.tsx
+++ src/mobile/components/map/Base.tsx
@@ -1,17 +1,12 @@
-import styled from 'styled-components';
-import BottomSheet from "./BottomSheet.tsx";
+import styled from "styled-components";
+// import BottomSheet from "./BottomSheet.tsx";
 
 const BaseDiv = styled.div`
-    height: 100vh;
-    width: 100vw;
-    background-color: #000000
-`
+  height: 100vh;
+  width: 100vw;
+  background-color: #000000;
+`;
 
 export default function Base() {
-
-    return (
-        <BaseDiv>
-            <BottomSheet/>
-        </BaseDiv>
-    )
+  return <BaseDiv>{/* <BottomSheet /> */}</BaseDiv>;
 }
