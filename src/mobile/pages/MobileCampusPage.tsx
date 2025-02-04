fc6430d08e97128fb108e8adcf9cd4fdae3319c8
2mingyu <tnt01010@naver.com>
2025-02-05 03:30:45 +0900

    fix: 캠퍼스 페이지 padding

--- src/mobile/pages/MobileCampusPage.tsx
+++ src/mobile/pages/MobileCampusPage.tsx
@@ -1,42 +1,36 @@
 import styled from "styled-components";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import MobileCampusHeader from "../components/map/MobileCampusHeader.tsx";
 import HelloBus from "../components/council/HelloBus.tsx";
 import MapManager from "components/map/MapManager.tsx";
 
 export default function MobileCampusPage() {
-    const location = useLocation();
-    const params = new URLSearchParams(location.search);
-    let type = params.get("type") || "campusmap";
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  let type = params.get("type") || "campusmap";
 
-
-    return (
-        <MobileTipsPageWrapper>
-            <MobileCampusHeader selectedType={type}/>
-            {type === "campusmap" && (
-                <>
-                    <MapManager/>
-                </>
-            )}
-            {type === "HelloBus" && (
-                <>
-                    <HelloBus/>
-                </>
-            )}
-
-        </MobileTipsPageWrapper>
-    );
+  return (
+    <MobileTipsPageWrapper>
+      <MobileCampusHeader selectedType={type} />
+      {type === "campusmap" && (
+        <>
+          <MapManager />
+        </>
+      )}
+      {type === "HelloBus" && (
+        <>
+          <HelloBus />
+        </>
+      )}
+    </MobileTipsPageWrapper>
+  );
 }
 
 const MobileTipsPageWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 16px;
-    padding: 0 16px 0 16px;
-    width: 100dvw;
-    height: 70dvh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  width: 100dvw;
 `;
-
-
