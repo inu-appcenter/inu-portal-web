40cbee5f8279a59d536141939efdf4ae434cdc3e
2mingyu <tnt01010@naver.com>
2025-02-05 03:39:28 +0900

    fix: 배포 오류 해결(Rental isOpen={false})

--- src/pages/RentalPage.tsx
+++ src/pages/RentalPage.tsx
@@ -3,50 +3,46 @@ import Categories from "components/posts/Categories";
 import RentalTitle from "../components/rental/components/RentalTitle.tsx";
 import styled from "styled-components";
 
-import {ThemeProvider} from "styled-components";
+import { ThemeProvider } from "styled-components";
 
 const theme = {
-    colors: {
-        primary: "#007bff",
-        secondary: "#f5f5f5",
-        text: "#333",
-        background: "#ffffff",
-        border: "#ddd",
-    },
+  colors: {
+    primary: "#007bff",
+    secondary: "#f5f5f5",
+    text: "#333",
+    background: "#ffffff",
+    border: "#ddd",
+  },
 };
 
 import Rental from "../../src/components/rental/Rental.tsx";
 
-
 export default function RentalPage() {
-
-    return (
-        <PostsPageWrapper>
-            <Categories/>
-            <ContentsWrapper>
-                <RentalTitle/>
-                <ThemeProvider theme={theme}>
-                    <Rental/>
-                </ThemeProvider>
-            </ContentsWrapper>
-        </PostsPageWrapper>
-    );
+  return (
+    <PostsPageWrapper>
+      <Categories />
+      <ContentsWrapper>
+        <RentalTitle />
+        <ThemeProvider theme={theme}>
+          <Rental isOpen={false} />
+        </ThemeProvider>
+      </ContentsWrapper>
+    </PostsPageWrapper>
+  );
 }
-
 
 //styled components
 
-
 const PostsPageWrapper = styled.div`
-    padding: 0 32px;
-    display: flex;
-    gap: 16px;
-    position: relative;
+  padding: 0 32px;
+  display: flex;
+  gap: 16px;
+  position: relative;
 `;
 
 const ContentsWrapper = styled.div`
-    flex: 1;
+  flex: 1;
 
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
