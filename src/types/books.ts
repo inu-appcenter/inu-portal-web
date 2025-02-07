e41dd89a99dae609927ebdeda9af8766ac8f01b8
baebaej <hinara12@inu.ac.kr>
2025-02-08 01:54:27 +0900

    fix: 배포 오류 수정

--- src/types/books.ts
+++ src/types/books.ts
@@ -1,25 +1,23 @@
 export interface BookSummary {
-  id: number;
-  name: string;
-  author: string;
-  thumbnail: string;
-  transactionStatus: string;
-  imageCount: number;
-  price: number;
-<<<<<<< Updated upstream
-  modifiedDate: string;
-=======
->>>>>>> Stashed changes
+    id: number;
+    name: string;
+    author: string;
+    thumbnail: string;
+    transactionStatus: string;
+    imageCount: number;
+    price: number;
+    modifiedDate: string;
+
 }
 
 export interface Book {
-  id: number;
-  name: string;
-  author: string;
-  price: number;
-  content: string;
-  transactionStatus: string;
-  imageCount: number;
-  image: string[];
-  modifiedDate: string;
+    id: number;
+    name: string;
+    author: string;
+    price: number;
+    content: string;
+    transactionStatus: string;
+    imageCount: number;
+    image: string[];
+    modifiedDate: string;
 }
