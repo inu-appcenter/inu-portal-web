a785f017b2ff5995da7e8f22248095f61a744859
2mingyu <tnt01010@naver.com>
2025-02-14 04:33:05 +0900

    feat: 모바일 회원탈퇴 제거

--- src/resources/strings/m-mypage.tsx
+++ src/resources/strings/m-mypage.tsx
@@ -4,7 +4,7 @@ import commentImg from "resources/assets/mobile-mypage/comment-img.svg";
 import scrapImg from "resources/assets/mobile-mypage/scrap.svg";
 import profileImg from "resources/assets/mobile-mypage/profile.svg";
 import logoutImg from "resources/assets/mobile-mypage/logout.svg";
-import removeImg from "resources/assets/mobile-mypage/remove.svg";
+// import removeImg from "resources/assets/mobile-mypage/remove.svg";
 export const MyPageActive = [
   { title: "내가 쓴 글", image: `${writeImg}` },
   { title: "좋아요 한 글", image: `${likeImg}` },
@@ -15,5 +15,5 @@ export const MyPageCategory = [
   { title: "프로필 편집", image: `${profileImg}` },
   { title: "스크랩", image: `${scrapImg}` },
   { title: "로그아웃", image: `${logoutImg}` },
-  { title: "회원탈퇴", image: `${removeImg}` },
+  // { title: "회원탈퇴", image: `${removeImg}` },
 ];
