6c0ad85e57fc308fc32b6f5c8aefb82ba44d2ada
baebaej <korea024504@gmail.com>
2025-02-19 22:21:59 +0900

    design: 바텀시트 높이를 화면의 절반으로 제한

--- src/mobile/components/map/BottomSheetOption.ts
+++ src/mobile/components/map/BottomSheetOption.ts
@@ -1,3 +1,3 @@
 export const MIN_Y = 200;
-export const MAX_Y = window.innerHeight - 200;
+export const MAX_Y = window.innerHeight / 2;
 export const BOTTOM_SHEET_HEIGHT = window.innerHeight - MIN_Y;
