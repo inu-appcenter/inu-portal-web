539d765db1c92a6db3089de42c2ebb185e6a34a9
baebaej <korea024504@gmail.com>
2025-02-25 06:19:46 +0900

    fix: 작성한 댓글 페이지 width 이상한 문제 해결 시도

--- src/mobile/pages/MobileMyPageComment.tsx
+++ src/mobile/pages/MobileMyPageComment.tsx
@@ -1,37 +1,39 @@
-import { useEffect, useState } from "react";
-import { getMembersReplies } from "apis/members";
-import { MembersReplies } from "types/members";
+import {useEffect, useState} from "react";
+import {getMembersReplies} from "apis/members";
+import {MembersReplies} from "types/members";
 import CommontTitle from "mobile/containers/mypage/Title";
 import styled from "styled-components";
 import Empty from "mobile/components/mypage/Empty";
 import CardComment from "mobile/containers/mypage/CardComment";
 
 export default function MobileMyPageComment() {
-  const [replyPost, setReplyPost] = useState<MembersReplies[]>([]);
+    const [replyPost, setReplyPost] = useState<MembersReplies[]>([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await getMembersReplies("date");
-      setReplyPost(response.data);
-    } catch (error) {
-      console.error("회원이 작성한 모든 댓글 가져오기 실패", error);
-    }
-  };
+    const fetchData = async () => {
+        try {
+            const response = await getMembersReplies("date");
+            setReplyPost(response.data);
+        } catch (error) {
+            console.error("회원이 작성한 모든 댓글 가져오기 실패", error);
+        }
+    };
 
-  return (
-    <MobileMyPageCommentWrapper>
-      <CommontTitle title={"작성한 댓글"} />
-      {replyPost.length === 0 ? (
-        <Empty />
-      ) : (
-        <CardComment posts={replyPost} onCommentsUpdate={fetchData} />
-      )}
-    </MobileMyPageCommentWrapper>
-  );
+    return (
+        <MobileMyPageCommentWrapper>
+            <CommontTitle title={"작성한 댓글"}/>
+            {replyPost.length === 0 ? (
+                <Empty/>
+            ) : (
+                <CardComment posts={replyPost} onCommentsUpdate={fetchData}/>
+            )}
+        </MobileMyPageCommentWrapper>
+    );
 }
 
-const MobileMyPageCommentWrapper = styled.div``;
+const MobileMyPageCommentWrapper = styled.div`
+    width: 100%;
+`;
