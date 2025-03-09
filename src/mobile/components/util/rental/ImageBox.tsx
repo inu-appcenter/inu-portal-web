65ac8071fb49275fd068d09ca1265b84cbe97f2c
baebaej <hinara12@inu.ac.kr>
2025-03-09 14:44:24 +0900

    design: 물품대여 썸네일 비율 변경, 세부정보 창 디자인 변경

--- src/mobile/components/util/rental/ImageBox.tsx
+++ src/mobile/components/util/rental/ImageBox.tsx
@@ -24,18 +24,18 @@ const ImageBox = ({src, alt}: ImageBoxProps) => {
 // Styled Components
 const ImageBoxWrapper = styled.div`
     width: 100%;
-    aspect-ratio: 1 / 0.8; /* 가로세로 비율 유지 */
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-    overflow: hidden;
-    border-radius: 8px; /* 선택 사항: 이미지 모서리 둥글게 */
 `;
 
 const StyledImg = styled.img`
     width: 100%;
     height: 100%;
-    object-fit: cover; /* 이미지 비율 유지하며 잘림 */
+    aspect-ratio: 1 / 0.8; /* 가로세로 비율 유지 */
+    overflow: hidden;
+    border-radius: 8px; /* 선택 사항: 이미지 모서리 둥글게 */
 `;
 
 export default ImageBox;
