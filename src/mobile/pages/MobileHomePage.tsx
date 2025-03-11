cd054e9fb3d375b7627eed1c142eeaac4879f71e
baebaej <korea024504@gmail.com>
2025-03-11 22:22:43 +0900

    design: 모바일홈 횃불이ai 바로가기가 접속시 바로 보이도록 상단으로 이동

--- src/mobile/pages/MobileHomePage.tsx
+++ src/mobile/pages/MobileHomePage.tsx
@@ -6,51 +6,51 @@ import TipForm from "mobile/containers/home/Tips";
 import AiForm from "mobile/containers/home/Ai";
 import NoticeForm from "mobile/containers/home/Notice";
 import AppcenterLogo from "resources/assets/appcenter-logo.svg";
-import { ReactSVG } from "react-svg";
+import {ReactSVG} from "react-svg";
 
 export default function MobileHomePage() {
-  return (
-    <MobileHomePageWrapper>
-      <WeatherForm />
-      <ContainerWrapper>
-        <SerachForm />
-        <CategoryForm />
-        <TipForm />
-        <AiForm />
-        <NoticeForm />
-      </ContainerWrapper>
-      <AppcenterLogoWrapper>
-        <ReactSVG src={AppcenterLogo} />
-      </AppcenterLogoWrapper>
-    </MobileHomePageWrapper>
-  );
+    return (
+        <MobileHomePageWrapper>
+            <WeatherForm/>
+            <ContainerWrapper>
+                <SerachForm/>
+                <CategoryForm/>
+                <AiForm/>
+                <TipForm/>
+                <NoticeForm/>
+            </ContainerWrapper>
+            <AppcenterLogoWrapper>
+                <ReactSVG src={AppcenterLogo}/>
+            </AppcenterLogoWrapper>
+        </MobileHomePageWrapper>
+    );
 }
 
 const MobileHomePageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 170px;
-  width: 100%;
-  position: relative;
+    display: flex;
+    flex-direction: column;
+    margin-top: 170px;
+    width: 100%;
+    position: relative;
 
-  .asdf {
-  }
+    .asdf {
+    }
 `;
 
 const ContainerWrapper = styled.div`
-  margin: 0 24px;
+    margin: 0 24px;
 `;
 
 const AppcenterLogoWrapper = styled.div`
-  background: linear-gradient(to bottom, white, rgb(170, 201, 238));
-  padding: 24px 0;
-  height: 32px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  svg {
-    width: 100%;
+    background: linear-gradient(to bottom, white, rgb(170, 201, 238));
+    padding: 24px 0;
     height: 32px;
-  }
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+        width: 100%;
+        height: 32px;
+    }
 `;
