0b6bf1b0e680aba6da389c839fff2b246b90f0f0
baebaej <hinara12@inu.ac.kr>
2025-03-11 23:12:50 +0900

    fix: 배포 오류 수정

--- src/mobile/pages/MobileMyPagePost.tsx
+++ src/mobile/pages/MobileMyPagePost.tsx
@@ -1,39 +1,42 @@
-import { useEffect, useState } from "react";
-import { getMembersPosts } from "apis/members";
+import {useEffect, useState} from "react";
+import {getMembersPosts} from "apis/members";
 import CommontTitle from "mobile/containers/mypage/Title";
 import styled from "styled-components";
 import Card from "mobile/containers/mypage/Card";
 import Empty from "mobile/components/mypage/Empty";
-import { Post } from "types/posts";
+import {Post} from "types/posts";
+import useMobileNavigate from "../../hooks/useMobileNavigate.ts";
 
 export default function MobileMyPagePost() {
-  const [postPost, setPostPost] = useState<Post[]>([]);
+    const [postPost, setPostPost] = useState<Post[]>([]);
+    const mobileNavigate = useMobileNavigate();
 
-  useEffect(() => {
-    fetchData();
-  }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await getMembersPosts("date");
-      setPostPost(response.data);
-    } catch (error) {
-      console.error("회원이 작성한 모든 글 가져오기 실패", error);
-    }
-  };
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  return (
-    <MobileMyPagePostWrapper>
-      <CommontTitle title={"내가 쓴 글"} />
-      {postPost.length === 0 ? (
-        <Empty />
-      ) : (
-        <Card post={postPost} onUpdate={fetchData} type="post" />
-      )}
-    </MobileMyPagePostWrapper>
-  );
+    const fetchData = async () => {
+        try {
+            const response = await getMembersPosts("date");
+            setPostPost(response.data);
+        } catch (error) {
+            console.error("회원이 작성한 모든 글 가져오기 실패", error);
+        }
+    };
+
+    return (
+        <MobileMyPagePostWrapper>
+            <CommontTitle title={"내가 쓴 글"} onback={() => mobileNavigate('/home')}/>
+            {postPost.length === 0 ? (
+                <Empty/>
+            ) : (
+                <Card post={postPost} onUpdate={fetchData} type="post"/>
+            )}
+        </MobileMyPagePostWrapper>
+    );
 }
 
 const MobileMyPagePostWrapper = styled.div`
-  width: 100%;
+    width: 100%;
 `;
