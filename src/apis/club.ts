0dff06b98f671ec5afc5c23b556c3f5b7571c88c
baebaej <hinara12@inu.ac.kr>
2025-03-28 01:29:47 +0900

    feat: 동아리 공고 등록시 모집중 여부 설정 기능

--- src/apis/club.ts
+++ src/apis/club.ts
@@ -16,3 +16,82 @@ export const getClubs = async (
     return response.data;
   }
 };
+<<<<<<< Updated upstream
+=======
+
+// 동아리 모집공고글 가져오기
+export const getClubRecruit = async (
+  clubId: string,
+): Promise<ApiResponse<ClubRecruit>> => {
+  const response = await axiosInstance.get<ApiResponse<ClubRecruit>>(
+    `/api/clubs/${clubId}`,
+  );
+  return response.data;
+};
+
+// 동아리 모집 공고 등록
+export const postClubRecruit = async (
+  clubId: number,
+  content: string,
+  is_recruiting: boolean,
+  images: File[],
+): Promise<ApiResponse<number>> => {
+  const formData = new FormData();
+
+  // Dto 형식에 맞는 데이터를 추가
+  const jsonData = {
+    recruit: content,
+    is_recruiting: is_recruiting, // 모집 중 여부
+  };
+  const jsonBlob = new Blob([JSON.stringify(jsonData)], {
+    type: "application/json",
+  });
+  formData.append("clubRecruitingRequestDto", jsonBlob);
+
+  images.forEach((image) => formData.append("images", image));
+
+  const response = await tokenInstance.post<ApiResponse<number>>(
+    `/api/clubs/${clubId}`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    },
+  );
+  return response.data;
+};
+
+// 동아리 모집 공고 수정
+export const putClubRecruit = async (
+  clubId: number,
+  content: string,
+  is_recruiting: boolean,
+  images: File[],
+): Promise<ApiResponse<number>> => {
+  const formData = new FormData();
+
+  // Dto 형식에 맞는 데이터를 추가
+  const jsonData = {
+    recruit: content,
+    is_recruiting: is_recruiting, // 모집 중 여부
+  };
+  const jsonBlob = new Blob([JSON.stringify(jsonData)], {
+    type: "application/json",
+  });
+  formData.append("clubRecruitingRequestDto", jsonBlob);
+
+  images.forEach((image) => formData.append("images", image));
+
+  const response = await tokenInstance.put<ApiResponse<number>>(
+    `/api/clubs/${clubId}`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    },
+  );
+  return response.data;
+};
+>>>>>>> Stashed changes
