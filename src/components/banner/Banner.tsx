7e20dd2adbcea07c98cf65df96b38274c3aea78c
hjunieee <hinara12@inu.ac.kr>
2025-05-27 22:52:25 +0900

    feat: 모바일 홈페이지 배너 기능 추가

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import { ReactNode } from "react";
+
+export default function Banner({
+  title,
+  imgsrc,
+  content,
+}: {
+  title: string;
+  imgsrc: string;
+  content: ReactNode;
+}) {
+  return (
+    <BannerWrapper>
+      <div className="title-img">
+        <span className="title">{title}</span>
+        {/* <img src={AiExample} alt="" /> */}
+      </div>
+      <img src={imgsrc} alt={"배너 이미지"} />
+      <span style={{ fontSize: "20px", lineHeight: "30px" }}>{content}</span>
+    </BannerWrapper>
+  );
+}
+
+const BannerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding: 16px;
+  color: #333366;
+  background-color: #f5f6fa;
+  border-radius: 12px;
+
+  .title-img {
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 6px;
+    margin-bottom: 6px;
+    display: flex;
+    justify-content: flex-start; /* 왼쪽 정렬 */
+  }
+
+  .title {
+    font-size: 28px;
+    font-weight: 700;
+    text-align: left;
+  }
+
+  img {
+    width: 100%;
+    border-radius: 8px;
+    object-fit: cover;
+  }
+
+  span {
+    font-size: 18px;
+    line-height: 28px;
+    text-align: left; /* 본문도 왼쪽 정렬 */
+  }
+`;
