6b9409729b21343cfb74e691953ee38bdc778cd4
hjunieee <hinara12@inu.ac.kr>
2025-08-16 00:27:07 +0900

    Feat: 캘린더 달력 위에서 이벤트 확인 가능하도록 개선

--- src/mobile/pages/MobileCalendarPage.tsx
+++ src/mobile/pages/MobileCalendarPage.tsx
@@ -5,9 +5,7 @@ export default function MobileCalendarPage() {
   return (
     <MobileCalendarPageWrapper>
       {/*<Title title={"학사일정"} onback={() => mobileNavigate("/home")} />*/}
-      <Wrapper>
-        <Calendarbar />
-      </Wrapper>
+      <Calendarbar />
     </MobileCalendarPageWrapper>
   );
 }
@@ -17,11 +15,8 @@ const MobileCalendarPageWrapper = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-`;
+  align-items: center;
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  padding: 24px 16px;
+  padding: 30px 16px;
   box-sizing: border-box;
 `;
