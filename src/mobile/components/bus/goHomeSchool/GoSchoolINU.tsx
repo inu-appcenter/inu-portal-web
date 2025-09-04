387fd661d4cfcbcea07837ee9b557d6ec034c607
naturch <dudbk2@naver.com>
2025-09-05 03:53:28 +0900

    refactor: 버스 상세 페이지 라우팅 수정

--- src/mobile/components/bus/goHomeSchool/GoSchoolINU.tsx
+++ src/mobile/components/bus/goHomeSchool/GoSchoolINU.tsx
@@ -13,7 +13,7 @@ export default function GoSchoolINU() {
     <PageWrapper>
       <BusStopHeader
         stopName="인천대입구역"
-        stopNotice={`※ 등교 시간에는 지하철에서 내리자마자 정류장까지 달려가는\n'인입런' 현상이 생길 정도로 혼잡합니다. 참고해주세요.`}
+        stopNotice={`※ 아침에는 지하철에서 내리자마자 정류장까지 달려가는\n인입런 현상이 생길 정도로 혼잡합니다. 참고해주세요!`}
         showInfoIcon={false}
       />
       <BusStopBox
