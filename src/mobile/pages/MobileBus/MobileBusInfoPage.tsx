338dae218c3ee1081af0afe10cd6160619334f63
hjunieee <korea024504@gmail.com>
2025-09-24 01:48:47 +0900

    feat: 캠퍼스맵, 인입런 api로그 기능

--- src/mobile/pages/MobileBus/MobileBusInfoPage.tsx
+++ src/mobile/pages/MobileBus/MobileBusInfoPage.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
+import { useEffect } from "react";
 import BusTabHeader from "mobile/components/bus/BusTabHeader";
 import GoSchoolINU from "../../components/bus/goHomeSchool/GoSchoolINU.tsx";
 import GoSchoolBIT from "../../components/bus/goHomeSchool/GoSchoolBIT.tsx";
@@ -10,56 +11,85 @@ import MichuholShuttle from "../../components/bus/shuttle/MichuholShuttle.tsx";
 import SubwayShuttle from "../../components/bus/shuttle/SubwayShuttle.tsx";
 import SchoolShuttle from "../../components/bus/shuttle/SchoolShuttle.tsx";
 import MobileHeader from "../../containers/common/MobileHeader.tsx";
+import {postApiLogs} from "../../../apis/members.ts";
 
 export default function BusInfoPage() {
-  const location = useLocation();
-  const query = new URLSearchParams(location.search);
-  const type = query.get("type") || "go-school";
-  const tab = query.get("tab");
-  //디폴트 탭
-  const defaultTab =
-    type === "go-school"
-      ? "INU"
-      : type === "go-home"
-        ? "main"
-        : "michuholShuttle";
-  const selectedTab = tab ?? defaultTab;
+    const location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const type = query.get("type") || "go-school";
+    const tab = query.get("tab");
 
-  return (
-    <BusInfoPageWrapper>
-      <MobileHeader
-        title={`${type === "go-school" ? "학교 갈래요" : type === "go-home" ? "집 갈래요" : type === "shuttle" ? "셔틀버스" : "인입런"}`}
-      />
-      <BusTabHeader Type={type} />
-      <ContentWrapper>
-        {type === "go-school" && selectedTab === "INU" && <GoSchoolINU />}
-        {type === "go-school" && selectedTab === "BIT" && <GoSchoolBIT />}
-        {type === "go-home" && selectedTab === "main" && <GoHomeMain />}
-        {type === "go-home" && selectedTab === "science" && <GoHomeScience />}
-        {type === "go-home" && selectedTab === "dorm" && <GoHomeDorm />}
-        {type === "shuttle" && selectedTab === "michuholShuttle" && (
-          <MichuholShuttle />
-        )}
-        {type === "shuttle" && selectedTab === "subwayShuttle" && (
-          <SubwayShuttle />
-        )}
-        {type === "shuttle" && selectedTab === "schoolShuttle" && (
-          <SchoolShuttle />
-        )}
-      </ContentWrapper>
-    </BusInfoPageWrapper>
-  );
+    // 디폴트 탭
+    const defaultTab =
+        type === "go-school"
+            ? "INU"
+            : type === "go-home"
+                ? "main"
+                : "michuholShuttle";
+    const selectedTab = tab ?? defaultTab;
+
+    useEffect(() => {
+        const logApi = async () => {
+            console.log("현재 type:", type);
+
+            if (type === "go-school") {
+                console.log("학교 가는 버스 탭 선택됨");
+                await postApiLogs("/api/buses/go-school");
+            } else if (type === "go-home") {
+                console.log("집 가는 버스 탭 선택됨");
+                await postApiLogs("/api/buses/go-home");
+            } else if (type === "shuttle") {
+                console.log("셔틀버스 탭 선택됨");
+                await postApiLogs("/api/buses/shuttle");
+            }
+        };
+
+        logApi();
+    }, [type]);
+
+    return (
+        <BusInfoPageWrapper>
+            <MobileHeader
+                title={`${
+                    type === "go-school"
+                        ? "학교 갈래요"
+                        : type === "go-home"
+                            ? "집 갈래요"
+                            : type === "shuttle"
+                                ? "셔틀버스"
+                                : "인입런"
+                }`}
+            />
+            <BusTabHeader Type={type} />
+            <ContentWrapper>
+                {type === "go-school" && selectedTab === "INU" && <GoSchoolINU />}
+                {type === "go-school" && selectedTab === "BIT" && <GoSchoolBIT />}
+                {type === "go-home" && selectedTab === "main" && <GoHomeMain />}
+                {type === "go-home" && selectedTab === "science" && <GoHomeScience />}
+                {type === "go-home" && selectedTab === "dorm" && <GoHomeDorm />}
+                {type === "shuttle" && selectedTab === "michuholShuttle" && (
+                    <MichuholShuttle />
+                )}
+                {type === "shuttle" && selectedTab === "subwayShuttle" && (
+                    <SubwayShuttle />
+                )}
+                {type === "shuttle" && selectedTab === "schoolShuttle" && (
+                    <SchoolShuttle />
+                )}
+            </ContentWrapper>
+        </BusInfoPageWrapper>
+    );
 }
 
 const BusInfoPageWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding-top: 56px;
-  box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding-top: 56px;
+    box-sizing: border-box;
 `;
 
 const ContentWrapper = styled.div`
-  padding: 0 16px;
-  box-sizing: border-box;
+    padding: 0 16px;
+    box-sizing: border-box;
 `;
