a970ed8e9a8bd4f010aa57224ef79aa6a4433dce
hjunieee <hinara12@inu.ac.kr>
2025-09-24 15:52:47 +0900

    fix: 팁게시글 좋아요 및 스크랩 버튼 부활

--- src/mobile/containers/postdetail/PostUtilContainer.tsx
+++ src/mobile/containers/postdetail/PostUtilContainer.tsx
@@ -1,11 +1,6 @@
 import styled from "styled-components";
 import PostLike from "mobile/components/postdetail/util/m.postlike";
 import PostScrap from "mobile/components/postdetail/util/m.postscrap";
-import utilfolder from "resources/assets/mobile-tips/utilfolder.svg";
-import { useEffect, useRef, useState } from "react";
-import DeletePostBtn from "mobile/components/postdetail/util/DeletePostBtn";
-import EditPostBtn from "mobile/components/postdetail/util/EditPostBtn";
-import ReportsPostBtn from "mobile/components/postdetail/util/ReportsPostBtn";
 
 interface PostUtilityProps {
   id: number;
@@ -13,7 +8,6 @@ interface PostUtilityProps {
   isLiked: boolean;
   scrap: number;
   isScraped: boolean;
-  hasAuthority: boolean;
 }
 
 export default function PostUtilContainer({
@@ -22,54 +16,12 @@ export default function PostUtilContainer({
   isLiked,
   scrap,
   isScraped,
-  hasAuthority,
 }: PostUtilityProps) {
-  const [showPopup, setShowPopup] = useState(false);
-  const popupRef = useRef<HTMLDivElement>(null);
-
-  const handleFolderClick = () => {
-    setShowPopup((prev) => !prev);
-  };
-  const handleClickOutside = (event: MouseEvent) => {
-    if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
-      setShowPopup(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-  const handlePostUpdate = () => {
-    //
-  };
-
   return (
     <>
       <UtilWrapper>
         <PostLike id={id} like={like} isLikedProp={isLiked} />
         <PostScrap id={id} scrap={scrap} isScrapedProp={isScraped} />
-        <DelOrModifyWrapper>
-          <img
-            src={utilfolder}
-            alt="del or modify folder"
-            onClick={handleFolderClick}
-            style={{ cursor: "pointer" }}
-          />
-          {showPopup && (
-            <Popup ref={popupRef}>
-              {hasAuthority && (
-                <>
-                  <DeletePostBtn id={id} onPostUpdate={handlePostUpdate} />
-                  <EditPostBtn id={id} />
-                </>
-              )}
-              <ReportsPostBtn id={id} />
-            </Popup>
-          )}
-        </DelOrModifyWrapper>
       </UtilWrapper>
     </>
   );
@@ -80,23 +32,4 @@ const UtilWrapper = styled.div`
   display: flex;
   flex-direction: row;
   gap: 20px;
-`;
-const DelOrModifyWrapper = styled.div`
-  img {
-    padding: 4px 8px 0 8px;
-  }
-`;
-
-const Popup = styled.div`
-  position: absolute;
-  top: 10%;
-  right: 12px;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  padding: 10px;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-  z-index: 10;
 `;
