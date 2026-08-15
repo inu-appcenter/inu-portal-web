76823bc68383064788c9145e72a72b77add763fc
hjunieee <hinara12@inu.ac.kr>
2026-08-15 16:21:39 +0900

    style: CategorySelectorNew 미사용 import 제거 및 빌드 검증 완료

--- src/components/mobile/common/CategorySelectorNew.tsx
+++ src/components/mobile/common/CategorySelectorNew.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { SOFT_CHIP_SHADOW } from "@/styles/shadows";
 
 interface CategoryOption {
   label: string;
